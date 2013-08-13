d8e3e62a5780af41e1a597a84e454ef6b5905f7d
Sergio González <sergio.gonzalez@liferay.com>
2013-08-13 18:11:05 +0200

    wiki wip

--- portal-impl/test/integration/com/liferay/portlet/wiki/trash/WikiPageTrashHandlerTest.java
+++ portal-impl/test/integration/com/liferay/portlet/wiki/trash/WikiPageTrashHandlerTest.java
@@ -50,11 +50,6 @@ import org.junit.runner.RunWith;
 public class WikiPageTrashHandlerTest extends BaseTrashHandlerTestCase {
 
 	@Override
-	public void testTrashAndDeleteDraft() throws Exception {
-		Assert.assertTrue("This test does not apply", true);
-	}
-
-	@Override
 	public void testTrashAndRestoreDraft() throws Exception {
 		Assert.assertTrue("This test does not apply", true);
 	}
@@ -147,12 +142,28 @@ public class WikiPageTrashHandlerTest extends BaseTrashHandlerTestCase {
 	}
 
 	@Override
+	protected int getNotInTrashBaseModelsCount(
+			BaseModel<?> parentBaseModel, boolean approved)
+		throws Exception {
+
+		boolean head = true;
+
+		if (!approved) {
+			head = false;
+		}
+
+		return WikiPageLocalServiceUtil.getPagesCount(
+			(Long)parentBaseModel.getPrimaryKeyObj(), head,
+			WorkflowConstants.STATUS_ANY);
+	}
+
+	@Override
 	protected int getNotInTrashBaseModelsCount(BaseModel<?> parentBaseModel)
 		throws Exception {
 
 		return WikiPageLocalServiceUtil.getPagesCount(
 			(Long)parentBaseModel.getPrimaryKeyObj(), true,
-			WorkflowConstants.STATUS_APPROVED);
+			WorkflowConstants.STATUS_ANY);
 	}
 
 	@Override
