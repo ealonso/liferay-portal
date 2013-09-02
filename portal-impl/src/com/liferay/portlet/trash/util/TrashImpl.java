d58e82e1af2d9f4280f375b42f6d79b9a575b047
Zsolt Berentey <zsolt.berentey@liferay.com>
2013-09-02 10:49:25 +0200

    LPS-39399 Fix TrashImpl

--- portal-impl/src/com/liferay/portlet/trash/util/TrashImpl.java
+++ portal-impl/src/com/liferay/portlet/trash/util/TrashImpl.java
@@ -450,11 +450,10 @@ public class TrashImpl implements Trash {
 			className);
 
 		if (trashHandler.isInTrashContainer(classPK)) {
-			ContainerModel containerModel = trashHandler.getTrashContainer(
-				classPK);
+			TrashEntry trashEntry = trashHandler.getTrashEntry(classPK);
 
-			className = containerModel.getModelClassName();
-			classPK = containerModel.getContainerModelId();
+			className = trashEntry.getClassName();
+			classPK = trashEntry.getClassPK();
 
 			trashHandler = TrashHandlerRegistryUtil.getTrashHandler(className);
 		}
@@ -586,8 +585,8 @@ public class TrashImpl implements Trash {
 
 			if (!containerModelTrashHandler.isInTrash(
 					containerModel.getContainerModelId()) &&
-				(containerModelTrashHandler.getTrashContainer(
-					containerModel.getContainerModelId()) == null)) {
+				!containerModelTrashHandler.isInTrashContainer(
+					containerModel.getContainerModelId())) {
 
 				continue;
 			}
