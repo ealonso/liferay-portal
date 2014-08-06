a490dd3c6ca1cbb4c083e5842e0dcd058f3f5272
Dale Shan <dale.shan@liferay.com>
2014-08-06 14:31:38 +0800

    LPS-48189 Auto generated

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/persistence/impl/DLFileEntryPersistenceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/persistence/impl/DLFileEntryPersistenceImpl.java
@@ -27,8 +27,6 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.CharPool;
-import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.InstanceFactory;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.kernel.util.StringBundler;
@@ -37,7 +35,6 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.uuid.PortalUUIDUtil;
 import com.liferay.portal.model.CacheModel;
-import com.liferay.portal.model.ModelListener;
 import com.liferay.portal.security.permission.InlineSQLHelperUtil;
 import com.liferay.portal.service.persistence.impl.BasePersistenceImpl;
 
@@ -49,7 +46,6 @@ import com.liferay.portlet.documentlibrary.service.persistence.DLFileEntryPersis
 
 import java.io.Serializable;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
