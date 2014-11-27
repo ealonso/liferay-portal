6cce7453d5413bdb3e7e378b9c387e581610fe34
Jorge Díaz <jorge.diaz@liferay.com>
2014-11-27 18:13:20 +0100

    LPS-51880 At doReindexDDMStructures, only reindex head version when "journal.articles.index.all.versions=true"

--- portal-impl/src/com/liferay/portlet/journal/util/JournalArticleIndexer.java
+++ portal-impl/src/com/liferay/portlet/journal/util/JournalArticleIndexer.java
@@ -556,7 +556,11 @@ public class JournalArticleIndexer extends BaseIndexer {
 				ddmStructureKeys);
 
 		for (JournalArticle article : articles) {
-			doReindex(article, false);
+			if (PropsValues.JOURNAL_ARTICLE_INDEX_ALL_VERSIONS ||
+				isHead(article)) {
+
+				doReindex(article, false);
+			}
 		}
 	}
 
