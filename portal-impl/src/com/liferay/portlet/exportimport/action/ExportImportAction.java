a2e81de95485ab75bb542c45fb3b4234178b2235
Juergen Kappler <jurgen.kappler@liferay.com>
2015-05-27 10:46:02 +0200

    LPS-55833 Fix import and export actions

--- portal-impl/src/com/liferay/portlet/exportimport/action/ExportImportAction.java
+++ portal-impl/src/com/liferay/portlet/exportimport/action/ExportImportAction.java
@@ -386,8 +386,8 @@ public class ExportImportAction extends ImportLayoutsAction {
 			ExportImportConfiguration exportImportConfiguration =
 				ExportImportConfigurationLocalServiceUtil.
 					addExportImportConfiguration(
-						themeDisplay.getUserId(), groupId, StringPool.BLANK,
-						StringPool.BLANK,
+						themeDisplay.getUserId(), groupId,
+						portlet.getPortletId(), StringPool.BLANK,
 						ExportImportConfigurationConstants.TYPE_EXPORT_PORTLET,
 						settingsMap, WorkflowConstants.STATUS_DRAFT,
 						new ServiceContext());
@@ -461,7 +461,7 @@ public class ExportImportAction extends ImportLayoutsAction {
 		ExportImportConfiguration exportImportConfiguration =
 			ExportImportConfigurationLocalServiceUtil.
 				addExportImportConfiguration(
-					themeDisplay.getUserId(), groupId, StringPool.BLANK,
+					themeDisplay.getUserId(), groupId, portlet.getPortletId(),
 					StringPool.BLANK,
 					ExportImportConfigurationConstants.TYPE_IMPORT_PORTLET,
 					settingsMap, WorkflowConstants.STATUS_DRAFT,
