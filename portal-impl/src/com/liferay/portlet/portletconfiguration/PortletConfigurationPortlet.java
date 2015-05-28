58880df6c8de51773dc3a799de76403011437f23
Juergen Kappler <jurgen.kappler@liferay.com>
2015-05-28 13:08:10 +0200

    LPS-55833 Change from struts portlet to MVC Portlet

--- portal-impl/src/com/liferay/portlet/portletconfiguration/PortletConfigurationPortlet.java
+++ portal-impl/src/com/liferay/portlet/portletconfiguration/PortletConfigurationPortlet.java
@@ -16,6 +16,7 @@ package com.liferay.portlet.portletconfiguration;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.util.AutoResetThreadLocal;
 import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -46,10 +47,26 @@ import javax.portlet.ResourceResponse;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.osgi.service.component.annotations.Component;
+
 /**
  * @author Carlos Sierra Andrés
  */
-public class PortletConfigurationPortlet extends StrutsPortlet {
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.system=true",
+		"com.liferay.portlet.use-default-template=false",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/edit_configuration.jsp",
+		"javax.portlet.resource-bundle=content.Language"
+	},
+	service = Portlet.class
+)
+public class PortletConfigurationPortlet extends MVCPortlet {
 
 	@Override
 	public void init(PortletConfig portletConfig) throws PortletException {
@@ -116,6 +133,14 @@ public class PortletConfigurationPortlet extends StrutsPortlet {
 			JavaConstants.JAVAX_PORTLET_CONFIG, getPortletConfig());
 
 		super.serveResource(resourceRequest, resourceResponse);
+	}
+
+	@Override
+	protected void doDispatch(
+		RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		super.doDispatch(renderRequest, renderResponse);
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
