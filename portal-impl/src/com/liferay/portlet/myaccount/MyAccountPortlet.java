4fcd5392d9d7f2a120ea8c14f6c89e6377317b9c
Pei-Jung Lan <pei-jung.lan@liferay.com>
2015-06-23 11:41:32 -0700

    LPS-55590 Copy EditUserAction to MyAccountPortlet, no logic changes

--- portal-impl/src/com/liferay/portlet/myaccount/MyAccountPortlet.java
+++ portal-impl/src/com/liferay/portlet/myaccount/MyAccountPortlet.java
@@ -20,4 +20,89 @@ import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
  * @author Pei-Jung Lan
  */
 public class MyAccountPortlet extends MVCPortlet {
+
+	@Override
+	public void processAction(
+			ActionMapping actionMapping, ActionForm actionForm,
+			PortletConfig portletConfig, ActionRequest actionRequest,
+			ActionResponse actionResponse)
+		throws Exception {
+
+		if (redirectToLogin(actionRequest, actionResponse)) {
+			return;
+		}
+
+		super.processAction(
+			actionMapping, actionForm, portletConfig, actionRequest,
+			actionResponse);
+	}
+
+	@Override
+	public ActionForward render(
+			ActionMapping actionMapping, ActionForm actionForm,
+			PortletConfig portletConfig, RenderRequest renderRequest,
+			RenderResponse renderResponse)
+		throws Exception {
+
+		User user = PortalUtil.getUser(renderRequest);
+
+		RenderRequestImpl renderRequestImpl = (RenderRequestImpl)renderRequest;
+
+		DynamicServletRequest dynamicRequest =
+			(DynamicServletRequest)renderRequestImpl.getHttpServletRequest();
+
+		dynamicRequest.setParameter(
+			"p_u_i_d", String.valueOf(user.getUserId()));
+
+		return super.render(
+			actionMapping, actionForm, portletConfig, renderRequest,
+			renderResponse);
+	}
+
+	@Override
+	protected Object[] updateUser(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		String currentPassword = actionRequest.getParameter("password0");
+		String newPassword = actionRequest.getParameter("password1");
+
+		User user = PortalUtil.getSelectedUser(actionRequest);
+
+		if (Validator.isNotNull(currentPassword)) {
+			if (Validator.isNull(newPassword)) {
+				throw new UserPasswordException.MustNotBeNull(user.getUserId());
+			}
+
+			Company company = PortalUtil.getCompany(actionRequest);
+
+			String authType = company.getAuthType();
+
+			String login = null;
+
+			if (authType.equals(CompanyConstants.AUTH_TYPE_EA)) {
+				login = user.getEmailAddress();
+			}
+			else if (authType.equals(CompanyConstants.AUTH_TYPE_ID)) {
+				login = String.valueOf(user.getUserId());
+			}
+			else if (authType.equals(CompanyConstants.AUTH_TYPE_SN)) {
+				login = user.getScreenName();
+			}
+
+			boolean validPassword = PwdAuthenticator.authenticate(
+				login, currentPassword, user.getPassword());
+
+			if (!validPassword) {
+				throw new UserPasswordException.MustMatchCurrentPassword(
+					user.getUserId());
+			}
+		}
+		else if (Validator.isNotNull(newPassword)) {
+			throw new UserPasswordException.MustNotBeNull(user.getUserId());
+		}
+
+		return super.updateUser(actionRequest, actionResponse);
+	}
+
 }
