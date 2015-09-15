229c887190213d778255bc020b2acfd9c37be428
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-09-15 12:52:00 +0200

    LPS-58550 Creates new row checker for lexicon views

--- portal-service/src/com/liferay/portal/kernel/dao/search/RowChecker.java
+++ portal-service/src/com/liferay/portal/kernel/dao/search/RowChecker.java
@@ -226,12 +226,37 @@ public class RowChecker {
 		return value;
 	}
 
+	protected String getOnClick(
+		String checkBoxRowIds, String checkBoxAllRowIds,
+		String checkBoxPostOnClick) {
+
+		StringBundler sb = new StringBundler(11);
+
+		sb.append("onClick=\"Liferay.Util.checkAllBox(");
+		sb.append("AUI().one(this).ancestor('");
+		sb.append(".table'), ");
+		sb.append(checkBoxRowIds);
+		sb.append(", ");
+		sb.append(checkBoxAllRowIds);
+		sb.append(");");
+		sb.append("AUI().one(this).ancestor('tr:not(.lfr-template)').");
+		sb.append("toggleClass('info');");
+
+		if (Validator.isNotNull(checkBoxPostOnClick)) {
+			sb.append(checkBoxPostOnClick);
+		}
+
+		sb.append("\"");
+
+		return sb.toString();
+	}
+
 	protected String getRowCheckBox(
 		HttpServletRequest request, boolean checked, boolean disabled,
 		String name, String value, String checkBoxRowIds,
 		String checkBoxAllRowIds, String checkBoxPostOnClick) {
 
-		StringBundler sb = new StringBundler(22);
+		StringBundler sb = new StringBundler(14);
 
 		sb.append("<input ");
 
@@ -254,21 +279,9 @@ public class RowChecker {
 		sb.append("\" ");
 
 		if (Validator.isNotNull(_allRowIds)) {
-			sb.append("onClick=\"Liferay.Util.checkAllBox(");
-			sb.append("AUI().one(this).ancestor('");
-			sb.append(".table'), ");
-			sb.append(checkBoxRowIds);
-			sb.append(", ");
-			sb.append(checkBoxAllRowIds);
-			sb.append(");");
-			sb.append("AUI().one(this).ancestor('tr:not(.lfr-template)').");
-			sb.append("toggleClass('info');");
-
-			if (Validator.isNotNull(checkBoxPostOnClick)) {
-				sb.append(checkBoxPostOnClick);
-			}
-
-			sb.append("\"");
+			sb.append(
+				getOnClick(
+					checkBoxRowIds, checkBoxAllRowIds, checkBoxPostOnClick));
 		}
 
 		sb.append(">");
