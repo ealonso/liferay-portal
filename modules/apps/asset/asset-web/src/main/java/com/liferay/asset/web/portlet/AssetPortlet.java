bf7e38a3466cbaeda116d570ce29a0ddf454b552
Juergen Kappler <jurgen.kappler@liferay.com>
2016-01-21 12:57:20 +0100

    LPS-62291 Register portlet as a component

--- modules/apps/asset/asset-web/src/main/java/com/liferay/asset/web/portlet/AssetPortlet.java
+++ modules/apps/asset/asset-web/src/main/java/com/liferay/asset/web/portlet/AssetPortlet.java
@@ -14,9 +14,33 @@
 
 package com.liferay.asset.web.portlet;
 
+import com.liferay.asset.web.constants.AssetPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
 /**
  * @author Jürgen Kappler
  */
-
-public class AssetPortlet {
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.display-category=category.hidden",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.struts-path=asset",
+		"com.liferay.portlet.system=true",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Asset", "javax.portlet.expiration-cache=0",
+		"javax.portlet.name=" + AssetPortletKeys.ASSET,
+		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+public class AssetPortlet extends MVCPortlet {
 }
