7d3e81bef038c2069a764d18659b747f77f09577
antoniopol06 <antonio.pol@liferay.com>
2016-06-14 11:31:41 +0200

    Apply source formatting to java class

--- modules/apps/web-experience/asset/asset-taglib/src/main/java/com/liferay/asset/taglib/servlet/taglib/AssetTagsSelectorTag.java
+++ modules/apps/web-experience/asset/asset-taglib/src/main/java/com/liferay/asset/taglib/servlet/taglib/AssetTagsSelectorTag.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.asset.taglib.servlet.taglib;
 
 import com.liferay.asset.taglib.servlet.ServletContextUtil;
@@ -14,7 +28,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.PageContext;
 
 /**
- * Created by antoniopol on 13/6/16.
+ * @author Antonio Pol
  */
 public class AssetTagsSelectorTag extends IncludeTag {
 
