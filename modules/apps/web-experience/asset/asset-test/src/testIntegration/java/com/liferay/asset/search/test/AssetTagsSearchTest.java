9e0039d4f09e8b7796e032b9f4120b1eebeaceee
Juergen Kappler <jurgen.kappler@liferay.com>
2017-04-26 12:25:49 +0200

    LPS-72124 Add asset tags search test class

--- modules/apps/web-experience/asset/asset-test/src/testIntegration/java/com/liferay/asset/search/test/AssetTagsSearchTest.java
+++ modules/apps/web-experience/asset/asset-test/src/testIntegration/java/com/liferay/asset/search/test/AssetTagsSearchTest.java
@@ -15,21 +15,37 @@
 package com.liferay.asset.search.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.asset.kernel.model.AssetTag;
+import com.liferay.asset.kernel.service.AssetTagLocalServiceUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.search.BaseModelSearchResult;
 import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.rule.Sync;
 import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.security.permission.SimplePermissionChecker;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -51,8 +67,17 @@ public class AssetTagsSearchTest {
 		setUpPermissionThreadLocal();
 		setUpPrincipalThreadLocal();
 
+		_group = GroupTestUtil.addGroup();
+
 		_permissionChecker = PermissionCheckerFactoryUtil.create(
 			TestPropsValues.getUser());
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			_group.getGroupId(), TestPropsValues.getUserId());
+
+		_serviceContext.setCompanyId(TestPropsValues.getCompanyId());
+
+		ServiceContextThreadLocal.pushServiceContext(_serviceContext);
 	}
 
 	@After
@@ -60,6 +85,67 @@ public class AssetTagsSearchTest {
 		PermissionThreadLocal.setPermissionChecker(_permissionChecker);
 
 		PrincipalThreadLocal.setName(_name);
+	}
+
+	@Test
+	public void testExactMatchSearch() throws Exception {
+		AssetTagLocalServiceUtil.addTag(
+			TestPropsValues.getUserId(), _group.getGroupId(), "tag",
+			_serviceContext);
+
+		AssetTagLocalServiceUtil.addTag(
+			TestPropsValues.getUserId(), _group.getGroupId(), "tag1",
+			_serviceContext);
+
+		BaseModelSearchResult<AssetTag> searchResult =
+			AssetTagLocalServiceUtil.searchTags(
+				new long[] {_group.getGroupId()}, "\"tag\"", 0, 20, null);
+
+		Assert.assertEquals(1, searchResult.getLength());
+	}
+
+	@Test
+	public void testNoMatchSearch() throws Exception {
+		AssetTagLocalServiceUtil.addTag(
+			TestPropsValues.getUserId(), _group.getGroupId(), "tag1",
+			_serviceContext);
+
+		AssetTagLocalServiceUtil.addTag(
+			TestPropsValues.getUserId(), _group.getGroupId(), "tag2",
+			_serviceContext);
+
+		BaseModelSearchResult<AssetTag> searchResult =
+			AssetTagLocalServiceUtil.searchTags(
+				new long[] {_group.getGroupId()}, "\"tag\"", 0, 20, null);
+
+		Assert.assertEquals(0, searchResult.getLength());
+	}
+
+	@Test
+	public void testPartialMatchSearch() throws Exception {
+		List<String> searchAssetTags = Arrays.asList("tag1", "tag2", "tag3");
+
+		for (String assetTag : searchAssetTags) {
+			AssetTagLocalServiceUtil.addTag(
+				TestPropsValues.getUserId(), _group.getGroupId(), assetTag,
+				_serviceContext);
+		}
+
+		BaseModelSearchResult<AssetTag> searchResult =
+			AssetTagLocalServiceUtil.searchTags(
+				new long[] {_group.getGroupId()}, "tag", 0, 20, null);
+
+		List<AssetTag> assetTags = searchResult.getBaseModels();
+
+		Stream<AssetTag> assetTagsStream = assetTags.stream();
+
+		Stream<String> assetTagsNamesStream = assetTagsStream.map(
+			assetTag -> assetTag.getName());
+
+		List<String> assetTagsNames = assetTagsNamesStream.collect(
+			Collectors.toList());
+
+		Assert.assertTrue(assetTagsNames.containsAll(searchAssetTags));
 	}
 
 	protected void setUpPermissionThreadLocal() throws Exception {
@@ -88,7 +174,11 @@ public class AssetTagsSearchTest {
 		PrincipalThreadLocal.setName(TestPropsValues.getUserId());
 	}
 
+	@DeleteAfterTestRun
+	private Group _group;
+
 	private String _name;
 	private PermissionChecker _permissionChecker;
+	private ServiceContext _serviceContext;
 
 }
