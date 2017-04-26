21a2f14cbb4c6785face6eefc94f7422940c5d18
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-04-26 17:12:05 +0200

    Uses new interface in staging-processes-web

--- modules/apps/web-experience/staging/staging-processes-web/src/main/java/com/liferay/staging/processes/web/internal/portlet/action/ViewPublishLayoutsMVCRenderCommand.java
+++ modules/apps/web-experience/staging/staging-processes-web/src/main/java/com/liferay/staging/processes/web/internal/portlet/action/ViewPublishLayoutsMVCRenderCommand.java
@@ -16,8 +16,10 @@ package com.liferay.staging.processes.web.internal.portlet.action;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
 import com.liferay.staging.constants.StagingProcessesPortletKeys;
+import com.liferay.trash.TrashHelper;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Daniel Kocsis
@@ -37,5 +39,13 @@ public class ViewPublishLayoutsMVCRenderCommand
 	protected String getPath() {
 		return "/view.jsp";
 	}
+
+	@Override
+	protected TrashHelper getTrashHelper() {
+		return _trashHelper;
+	}
+
+	@Reference
+	private TrashHelper _trashHelper;
 
 }
