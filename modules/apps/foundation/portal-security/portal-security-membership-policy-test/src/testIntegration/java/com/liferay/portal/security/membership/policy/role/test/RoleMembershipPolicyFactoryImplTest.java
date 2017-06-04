3162013bbf94a2dfc92adeb93a4758f8ec5b2976
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-06-04 10:37:33 +0200

    LPS-x No needed

--- modules/apps/foundation/portal-security/portal-security-membership-policy-test/src/testIntegration/java/com/liferay/portal/security/membership/policy/role/test/RoleMembershipPolicyFactoryImplTest.java
+++ modules/apps/foundation/portal-security/portal-security-membership-policy-test/src/testIntegration/java/com/liferay/portal/security/membership/policy/role/test/RoleMembershipPolicyFactoryImplTest.java
@@ -22,7 +22,6 @@ import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.model.impl.RoleImpl;
 import com.liferay.portal.security.membership.policy.role.bundle.rolemembershippolicyfactoryimpl.TestRoleMembershipPolicy;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portal.test.rule.SyntheticBundleRule;
 import com.liferay.portal.util.test.AtomicState;
 
 import java.io.Serializable;
@@ -44,9 +43,7 @@ public class RoleMembershipPolicyFactoryImplTest {
 	@ClassRule
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
-		new AggregateTestRule(
-			new LiferayIntegrationTestRule(),
-			new SyntheticBundleRule("bundle.rolemembershippolicyfactoryimpl"));
+		new LiferayIntegrationTestRule();
 
 	@BeforeClass
 	public static void setUpClass() {
