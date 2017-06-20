47a3f8630e74ed270ee89249fdfdd2e4c04f7e5f
Jonathan Mak <jonathan.mak@liferay.com>
2017-06-20 11:50:04 -0700

    LPS-65792 - Remove add/remove of CSS class since those are added by the Bootstrap collapse() method.  Just call the Bootstrap method to show the categories when searching.

--- modules/apps/foundation/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/panel_search.js
+++ modules/apps/foundation/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/panel_search.js
@@ -153,16 +153,20 @@ AUI.add(
 
 									var contentParent = node.ancestorsByClassName(instance.get('categorySelector'));
 
-									var openCategories = $('.list-group-heading');
-									openCategories.collapse('show');
-									openCategories = $('.list-group-panel');
-									openCategories.collapse('show');
-
 									if (contentParent) {
 										contentParent.show();
 
-										contentParent.all('> .list-group-heading').removeClass('collapsed');
-										contentParent.all('> .list-group-panel').addClass('in');
+										var heading = contentParent.all('> .list-group-heading');
+
+										heading = heading.getDOM();
+
+										AUI.$(heading).collapse('show');
+
+										var panel = contentParent.all('> .list-group-panel');
+
+										panel = panel.getDOM();
+
+										AUI.$(panel).collapse('show');
 									}
 								}
 							);
