ff7235cdffcb786fbb1278bec684cff9bf15e67c
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-07-01 18:24:03 +0100

    LPS-74600 Deprecate classes

--- portal-impl/src/com/liferay/portlet/asset/model/impl/AssetEntryImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/model/impl/AssetEntryImpl.java
@@ -30,7 +30,10 @@ import java.util.List;
 /**
  * @author Brian Wing Shun Chan
  * @author Juan Fernández
+ * @deprecated As of 7.0.0, replaced by {@link
+ *            com.liferay.asset.model.impl.AssetEntryImpl}
  */
+@Deprecated
 public class AssetEntryImpl extends AssetEntryBaseImpl {
 
 	@Override
