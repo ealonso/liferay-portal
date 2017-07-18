072e975dc1f99e462a0eddb5e4ba705ccd850a22
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-07-18 16:22:02 +0200

    Asset - Removes illegal call to persistence

--- modules/apps/web-experience/asset/asset-service/src/main/java/com/liferay/asset/service/persistence/impl/AssetEntryFinderImpl.java
+++ modules/apps/web-experience/asset/asset-service/src/main/java/com/liferay/asset/service/persistence/impl/AssetEntryFinderImpl.java
@@ -15,7 +15,7 @@
 package com.liferay.asset.service.persistence.impl;
 
 import com.liferay.asset.categories.model.AssetCategory;
-import com.liferay.asset.categories.service.persistence.AssetCategoryUtil;
+import com.liferay.asset.categories.service.AssetCategoryLocalServiceUtil;
 import com.liferay.asset.model.AssetEntry;
 import com.liferay.asset.model.impl.AssetEntryImpl;
 import com.liferay.asset.service.persistence.AssetEntryFinder;
@@ -759,15 +759,15 @@ public class AssetEntryFinderImpl
 	}
 
 	protected List<Long> getSubcategoryIds(long parentCategoryId) {
-		AssetCategory parentAssetCategory = AssetCategoryUtil.fetchByPrimaryKey(
-			parentCategoryId);
+		AssetCategory parentAssetCategory =
+			AssetCategoryLocalServiceUtil.fetchAssetCategory(parentCategoryId);
 
 		if (parentAssetCategory == null) {
 			return Collections.emptyList();
 		}
 
 		return ListUtil.toList(
-			AssetCategoryUtil.getDescendants(parentAssetCategory),
+			AssetCategoryLocalServiceUtil.getChildCategories(parentCategoryId),
 			AssetCategory.CATEGORY_ID_ACCESSOR);
 	}
 
