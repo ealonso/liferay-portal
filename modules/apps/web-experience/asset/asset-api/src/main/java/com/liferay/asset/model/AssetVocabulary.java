638a0719e48cd3f6f3b96354ddfe6231c9983fd5
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-07-20 14:00:07 +0100

    LPS-74600 Auto

---
+++ modules/apps/web-experience/asset/asset-api/src/main/java/com/liferay/asset/model/AssetVocabulary.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.model;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.annotation.ImplementationClassName;
+import com.liferay.portal.kernel.model.PersistedModel;
+import com.liferay.portal.kernel.util.Accessor;
+
+/**
+ * The extended model interface for the AssetVocabulary service. Represents a row in the &quot;AssetVocabulary&quot; database table, with each column mapped to a property of this class.
+ *
+ * @author Brian Wing Shun Chan
+ * @see AssetVocabularyModel
+ * @see com.liferay.asset.model.impl.AssetVocabularyImpl
+ * @see com.liferay.asset.model.impl.AssetVocabularyModelImpl
+ * @generated
+ */
+@ImplementationClassName("com.liferay.asset.model.impl.AssetVocabularyImpl")
+@ProviderType
+public interface AssetVocabulary extends AssetVocabularyModel, PersistedModel {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this interface directly. Add methods to {@link com.liferay.asset.model.impl.AssetVocabularyImpl} and rerun ServiceBuilder to automatically copy the method declarations to this interface.
+	 */
+	public static final Accessor<AssetVocabulary, Long> VOCABULARY_ID_ACCESSOR = new Accessor<AssetVocabulary, Long>() {
+			@Override
+			public Long get(AssetVocabulary assetVocabulary) {
+				return assetVocabulary.getVocabularyId();
+			}
+
+			@Override
+			public Class<Long> getAttributeClass() {
+				return Long.class;
+			}
+
+			@Override
+			public Class<AssetVocabulary> getTypeClass() {
+				return AssetVocabulary.class;
+			}
+		};
+}
---
+++ modules/apps/web-experience/asset/asset-service/src/main/resources/META-INF/sql/tables.sql
@@ -0,0 +1,124 @@
+create table AssetCategory (
+	uuid_ VARCHAR(75) null,
+	categoryId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	parentCategoryId LONG,
+	leftCategoryId LONG,
+	rightCategoryId LONG,
+	name VARCHAR(75) null,
+	title STRING null,
+	description STRING null,
+	vocabularyId LONG,
+	lastPublishDate DATE null
+);
+
+create table AssetCategoryProperty (
+	categoryPropertyId LONG not null primary key,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	categoryId LONG,
+	key_ VARCHAR(75) null,
+	value VARCHAR(75) null
+);
+
+create table AssetEntries_AssetCategories (
+	companyId LONG not null,
+	categoryId LONG not null,
+	entryId LONG not null,
+	primary key (categoryId, entryId)
+);
+
+create table AssetEntries_AssetTags (
+	companyId LONG not null,
+	entryId LONG not null,
+	tagId LONG not null,
+	primary key (entryId, tagId)
+);
+
+create table AssetEntry (
+	entryId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	classNameId LONG,
+	classPK LONG,
+	classUuid VARCHAR(75) null,
+	classTypeId LONG,
+	listable BOOLEAN,
+	visible BOOLEAN,
+	startDate DATE null,
+	endDate DATE null,
+	publishDate DATE null,
+	expirationDate DATE null,
+	mimeType VARCHAR(75) null,
+	title STRING null,
+	description STRING null,
+	summary STRING null,
+	url VARCHAR(75) null,
+	layoutUuid VARCHAR(75) null,
+	height INTEGER,
+	width INTEGER,
+	priority DOUBLE,
+	viewCount INTEGER
+);
+
+create table AssetLink (
+	linkId LONG not null primary key,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	entryId1 LONG,
+	entryId2 LONG,
+	type_ INTEGER,
+	weight INTEGER
+);
+
+create table AssetTag (
+	uuid_ VARCHAR(75) null,
+	tagId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	name VARCHAR(75) null,
+	assetCount INTEGER,
+	lastPublishDate DATE null
+);
+
+create table AssetTagStats (
+	tagStatsId LONG not null primary key,
+	companyId LONG,
+	tagId LONG,
+	classNameId LONG,
+	assetCount INTEGER
+);
+
+create table AssetVocabulary (
+	uuid_ VARCHAR(75) null,
+	vocabularyId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	name VARCHAR(75) null,
+	title STRING null,
+	description STRING null,
+	settings_ VARCHAR(75) null,
+	lastPublishDate DATE null
+);
