0360bc870c0ac61d20085e968f2ca6d4f8a58108
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-08-01 14:26:36 +0100

    LPS-74600 Copy logic from core

--- modules/apps/web-experience/asset/asset-service/src/main/java/com/liferay/asset/model/impl/AssetLinkImpl.java
+++ modules/apps/web-experience/asset/asset-service/src/main/java/com/liferay/asset/model/impl/AssetLinkImpl.java
@@ -14,26 +14,8 @@
 
 package com.liferay.asset.model.impl;
 
-import aQute.bnd.annotation.ProviderType;
-
 /**
- * The extended model implementation for the AssetLink service. Represents a row in the &quot;AssetLink&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.asset.model.AssetLink} interface.
- * </p>
- *
  * @author Brian Wing Shun Chan
  */
-@ProviderType
 public class AssetLinkImpl extends AssetLinkBaseImpl {
-
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a asset link model instance should use the {@link com.liferay.asset.model.AssetLink} interface instead.
-	 */
-	public AssetLinkImpl() {
-	}
-
 }
