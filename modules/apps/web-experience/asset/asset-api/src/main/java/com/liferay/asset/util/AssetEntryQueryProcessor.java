6bf5fe4393f9ce95db0e9c387cfac79a72fe5fbb
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-09-01 10:37:12 +0200

    Asset API - Update imports

--- modules/apps/web-experience/asset/asset-api/src/main/java/com/liferay/asset/util/AssetEntryQueryProcessor.java
+++ modules/apps/web-experience/asset/asset-api/src/main/java/com/liferay/asset/util/AssetEntryQueryProcessor.java
@@ -14,7 +14,7 @@
 
 package com.liferay.asset.util;
 
-import com.liferay.asset.kernel.service.persistence.AssetEntryQuery;
+import com.liferay.asset.service.persistence.AssetEntryQuery;
 import com.liferay.portal.kernel.model.User;
 
 import java.io.IOException;
