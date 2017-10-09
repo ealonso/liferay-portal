d5c41e95cd969249289826424298d22961e58fdd
Pavel Savinov <pavel.savinov@liferay.com>
2017-10-09 16:09:06 +0200

    LPS-75021 Support drag & drop for site menu items container

--- modules/apps/web-experience/site-navigation/site-navigation-admin-web/src/main/resources/META-INF/resources/js/NavigationMenuTree.es.js
+++ modules/apps/web-experience/site-navigation/site-navigation-admin-web/src/main/resources/META-INF/resources/js/NavigationMenuTree.es.js
@@ -1,3 +1,4 @@
+import {Config} from 'metal-state';
 import {dom} from 'metal-dom';
 import Soy from 'metal-soy';
 
@@ -7,7 +8,6 @@ import templates from './NavigationMenuTree.soy';
 
 /**
  * NavigationMenuTree
- *
  */
 class NavigationMenuTree extends SelectLayout {
 
@@ -15,7 +15,30 @@ class NavigationMenuTree extends SelectLayout {
 	 * @inheritDoc
 	 */
 	attached() {
-		dom.on('.navigation-menu-tree .card', 'keyup', this._handleNodeKeyUp.bind(this))
+		dom.on(
+			'.navigation-menu-tree .card', 'keyup',
+			this._handleNodeKeyUp.bind(this));
+
+		dom.on(
+			'.navigation-menu-tree .card', 'dblclick',
+			this._handleNodeDblClick.bind(this));
+	}
+
+	/**
+	 * This is called when one of this tree view's nodes receives a double
+	 * click.
+	 *
+	 * @param {!Event} event
+	 * @protected
+	 */
+	_handleNodeDblClick(event) {
+		const target = event.delegateTarget.parentNode.parentNode.parentNode;
+		const treeView = this.refs.treeView;
+
+		const node = treeView.getNodeObj(
+			target.dataset.treeviewPath.split('-'));
+
+		this._selectItem(node);
 	}
 
 	/**
@@ -28,19 +51,109 @@ class NavigationMenuTree extends SelectLayout {
 		const target = event.delegateTarget.parentNode.parentNode.parentNode;
 		const treeView = this.refs.treeView;
 
-		const node = treeView.getNodeObj(target.dataset.treeviewPath.split('-'));
+		const node = treeView.getNodeObj(
+			target.dataset.treeviewPath.split('-'));
 
 		if (event.keyCode === 13) {
-			this.emit(
-				'itemSelected',
-				node
-			);
+			this._selectItem(node);
 		}
+	}
+
+	/**
+	 * This is called to select an item in the tree and add selected item to
+	 * the container.
+	 *
+	 * @param {!object} item Selected item object.
+	 * @private
+	 */
+	_selectItem(item) {
+		if (item.disabled) {
+			return;
+		}
+
+		this.emit('itemSelected', item);
+
+		this.nodes.forEach(
+			(node) => {
+				if (node.id == item.id) {
+					node.disabled = true;
+				}
+			}
+		);
 	}
 
 }
 
+let nodeShape = {
+	id: Config.string().value(''),
+	label: Config.string().required(),
+	name: Config.string().required(),
+	value: Config.string().value('')
+};
+
+const nodesValidator = Config.arrayOf(Config.shapeOf(nodeShape));
+
+nodeShape.children = nodesValidator;
+
 NavigationMenuTree.STATE = {
+
+	/**
+	 * Enables URL following on the title click
+	 *
+	 * @instance
+	 * @memberOf NavigationMenuTree
+	 * @type {?String}
+	 * @default false
+	 */
+	followURLOnTitleClick: Config.bool().value(false),
+
+	/**
+	 * Event name to fire on node selection
+	 *
+	 * @instance
+	 * @memberOf NavigationMenuTree
+	 * @type {!String}
+	 */
+	itemSelectorSaveEvent: Config.string().required(),
+
+	/**
+	 * List of nodes
+	 *
+	 * @instance
+	 * @memberOf NavigationMenuTree
+	 * @type {Array.<Object>}
+	 */
+	nodes: nodesValidator.required(),
+
+	/**
+	 * Enables multiple selection of tree elements
+	 *
+	 * @instance
+	 * @memberOf NavigationMenuTree
+	 * @type {?boolean}
+	 * @default false
+	 */
+	multiSelection: Config.bool().value(false),
+
+	/**
+	 * Theme images root path
+	 *
+	 * @instance
+	 * @memberOf NavigationMenuTree
+	 * @type {!String}
+	 */
+	pathThemeImages: Config.string().required(),
+
+	/**
+	 * Type of view to render. Accepted values are 'tree' and 'flat'
+	 *
+	 * @instance
+	 * @memberOf NavigationMenuTree
+	 * @type {?String}
+	 * @default tree
+	 */
+	viewType: Config.string().value('tree')
+
 };
 
 Soy.register(NavigationMenuTree, templates);
