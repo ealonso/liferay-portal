59b50d4583dc9d6640f5b84badfcba31606bbe49
Jürgen Kappler <jurgen.kappler@liferay.com>
2017-10-17 16:49:57 +0200

    LPS-75286 Replace service implementations with folder ones and adapt

--- modules/apps/web-experience/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/service/impl/LayoutPageTemplateCollectionLocalServiceImpl.java
+++ modules/apps/web-experience/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/service/impl/LayoutPageTemplateCollectionLocalServiceImpl.java
@@ -14,28 +14,192 @@
 
 package com.liferay.layout.page.template.service.impl;
 
+import com.liferay.layout.page.template.exception.DuplicateLayoutPageTemplateCollectionException;
+import com.liferay.layout.page.template.exception.LayoutPageTemplateCollectionNameException;
+import com.liferay.layout.page.template.model.LayoutPageTemplateCollection;
+import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.layout.page.template.service.base.LayoutPageTemplateCollectionLocalServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 
 /**
- * The implementation of the layout page template collection local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.layout.page.template.service.LayoutPageTemplateCollectionLocalService} interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
- * @author Brian Wing Shun Chan
- * @see LayoutPageTemplateCollectionLocalServiceBaseImpl
- * @see com.liferay.layout.page.template.service.LayoutPageTemplateCollectionLocalServiceUtil
+ * @author Jürgen Kappler
  */
 public class LayoutPageTemplateCollectionLocalServiceImpl
 	extends LayoutPageTemplateCollectionLocalServiceBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Always use {@link com.liferay.layout.page.template.service.LayoutPageTemplateCollectionLocalServiceUtil} to access the layout page template collection local service.
-	 */
+	@Override
+	public LayoutPageTemplateCollection addLayoutPageTemplateCollection(
+			long userId, long groupId, String name, String description,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		// Layout page template collection
+
+		User user = userLocalService.getUser(userId);
+
+		validate(groupId, name);
+
+		long layoutPageTemplateId = counterLocalService.increment();
+
+		LayoutPageTemplateCollection layoutPageTemplateCollection =
+			layoutPageTemplateCollectionPersistence.create(
+				layoutPageTemplateId);
+
+		layoutPageTemplateCollection.setGroupId(groupId);
+		layoutPageTemplateCollection.setCompanyId(user.getCompanyId());
+		layoutPageTemplateCollection.setUserId(user.getUserId());
+		layoutPageTemplateCollection.setUserName(user.getFullName());
+		layoutPageTemplateCollection.setCreateDate(
+			serviceContext.getCreateDate(new Date()));
+		layoutPageTemplateCollection.setModifiedDate(
+			serviceContext.getModifiedDate(new Date()));
+		layoutPageTemplateCollection.setName(name);
+		layoutPageTemplateCollection.setDescription(description);
+
+		layoutPageTemplateCollectionPersistence.update(
+			layoutPageTemplateCollection);
+
+		// Resources
+
+		resourceLocalService.addModelResources(
+			layoutPageTemplateCollection, serviceContext);
+
+		return layoutPageTemplateCollection;
+	}
+
+	@Override
+	public LayoutPageTemplateCollection deleteLayoutPageTemplateCollection(
+			LayoutPageTemplateCollection layoutPageTemplateCollection)
+		throws PortalException {
+
+		/// Layout page template collection
+
+		layoutPageTemplateCollectionPersistence.remove(
+			layoutPageTemplateCollection);
+
+		// Resources
+
+		resourceLocalService.deleteResource(
+			layoutPageTemplateCollection.getCompanyId(),
+			LayoutPageTemplateCollection.class.getName(),
+			ResourceConstants.SCOPE_INDIVIDUAL,
+			layoutPageTemplateCollection.getLayoutPageTemplateCollectionId());
+
+		// Layout page template entries
+
+		List<LayoutPageTemplateEntry> layoutPageTemplateEntries =
+			layoutPageTemplateEntryLocalService.getLayoutPageTemplateEntries(
+				layoutPageTemplateCollection.getGroupId(),
+				layoutPageTemplateCollection.
+					getLayoutPageTemplateCollectionId());
+
+		for (LayoutPageTemplateEntry layoutPageTemplateEntry :
+				layoutPageTemplateEntries) {
+
+			layoutPageTemplateEntryLocalService.deleteLayoutPageTemplateEntry(
+				layoutPageTemplateEntry);
+		}
+
+		return layoutPageTemplateCollection;
+	}
+
+	@Override
+	public LayoutPageTemplateCollection deleteLayoutPageTemplateCollection(
+			long layoutPageTemplateCollectionId)
+		throws PortalException {
+
+		LayoutPageTemplateCollection layoutPageTemplateCollection =
+			getLayoutPageTemplateCollection(layoutPageTemplateCollectionId);
+
+		return deleteLayoutPageTemplateCollection(layoutPageTemplateCollection);
+	}
+
+	@Override
+	public LayoutPageTemplateCollection fetchLayoutPageTemplateCollection(
+		long layoutPageTemplateCollectionId) {
+
+		return layoutPageTemplateCollectionPersistence.fetchByPrimaryKey(
+			layoutPageTemplateCollectionId);
+	}
+
+	@Override
+	public List<LayoutPageTemplateCollection> getLayoutPageTemplateCollections(
+			long groupId, int start, int end)
+		throws PortalException {
+
+		return layoutPageTemplateCollectionPersistence.findByGroupId(
+			groupId, start, end);
+	}
+
+	@Override
+	public List<LayoutPageTemplateCollection> getLayoutPageTemplateCollections(
+			long groupId, int start, int end,
+			OrderByComparator<LayoutPageTemplateCollection> orderByComparator)
+		throws PortalException {
+
+		return layoutPageTemplateCollectionPersistence.findByGroupId(
+			groupId, start, end, orderByComparator);
+	}
+
+	@Override
+	public List<LayoutPageTemplateCollection> getLayoutPageTemplateCollections(
+		long groupId, String name, int start, int end,
+		OrderByComparator<LayoutPageTemplateCollection> orderByComparator) {
+
+		if (Validator.isNull(name)) {
+			return layoutPageTemplateCollectionPersistence.findByGroupId(
+				groupId, start, end, orderByComparator);
+		}
+
+		return layoutPageTemplateCollectionPersistence.findByG_LikeN(
+			groupId, name, start, end, orderByComparator);
+	}
+
+	@Override
+	public LayoutPageTemplateCollection updateLayoutPageTemplateCollection(
+			long layoutPageTemplateCollectionId, String name,
+			String description)
+		throws PortalException {
+
+		LayoutPageTemplateCollection layoutPageTemplateCollection =
+			layoutPageTemplateCollectionPersistence.findByPrimaryKey(
+				layoutPageTemplateCollectionId);
+
+		if (!Objects.equals(layoutPageTemplateCollection.getName(), name)) {
+			validate(layoutPageTemplateCollection.getGroupId(), name);
+		}
+
+		layoutPageTemplateCollection.setModifiedDate(new Date());
+		layoutPageTemplateCollection.setName(name);
+		layoutPageTemplateCollection.setDescription(description);
+
+		layoutPageTemplateCollectionPersistence.update(
+			layoutPageTemplateCollection);
+
+		return layoutPageTemplateCollection;
+	}
+
+	protected void validate(long groupId, String name) throws PortalException {
+		if (Validator.isNull(name)) {
+			throw new LayoutPageTemplateCollectionNameException(
+				"Name must not be null for group " + groupId);
+		}
+
+		LayoutPageTemplateCollection layoutPageTemplateCollection =
+			layoutPageTemplateCollectionPersistence.fetchByG_N(groupId, name);
+
+		if (layoutPageTemplateCollection != null) {
+			throw new DuplicateLayoutPageTemplateCollectionException(name);
+		}
+	}
+
 }
