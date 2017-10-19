115e724a5baa1519c5646221db8c5c432ed19158
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-10-19 13:49:23 +0200

    LPS-74600 Group - Removes dependencies asset creating new wrappers

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -407,14 +407,6 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 
 			userLocalService.addGroupUsers(
 				group.getGroupId(), new long[] {userId});
-
-			// Asset
-
-			if (serviceContext != null) {
-				updateAsset(
-					userId, group, serviceContext.getAssetCategoryIds(),
-					serviceContext.getAssetTagNames());
-			}
 		}
 
 		addPortletDefaultData(group);
@@ -774,19 +766,6 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 			// Portlet data
 
 			deletePortletData(group);
-
-			// Asset
-
-			if (group.isRegularSite()) {
-				assetEntryLocalService.deleteEntry(
-					Group.class.getName(), group.getGroupId());
-			}
-
-			assetEntryLocalService.deleteGroupEntries(group.getGroupId());
-
-			assetTagLocalService.deleteGroupTags(group.getGroupId());
-
-			assetVocabularyLocalService.deleteVocabularies(group.getGroupId());
 
 			// Expando
 
@@ -3176,20 +3155,6 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 			long userId, Group group, long[] assetCategoryIds,
 			String[] assetTagNames)
 		throws PortalException {
-
-		User user = userPersistence.findByPrimaryKey(userId);
-
-		Company company = companyPersistence.findByPrimaryKey(
-			user.getCompanyId());
-
-		Group companyGroup = company.getGroup();
-
-		assetEntryLocalService.updateEntry(
-			userId, companyGroup.getGroupId(), null, null,
-			Group.class.getName(), group.getGroupId(), null, 0,
-			assetCategoryIds, assetTagNames, true, false, null, null, null,
-			null, null, group.getDescriptiveName(), group.getDescription(),
-			null, null, null, 0, 0, null);
 	}
 
 	/**
@@ -3326,29 +3291,6 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 
 			groupPersistence.update(stagingGroup);
 		}
-
-		// Asset
-
-		if ((serviceContext == null) || !group.isSite()) {
-			return group;
-		}
-
-		User user = null;
-
-		user = userPersistence.fetchByPrimaryKey(group.getCreatorUserId());
-
-		if (user == null) {
-			user = userPersistence.fetchByPrimaryKey(
-				serviceContext.getUserId());
-		}
-
-		if (user == null) {
-			user = userLocalService.getDefaultUser(group.getCompanyId());
-		}
-
-		updateAsset(
-			user.getUserId(), group, serviceContext.getAssetCategoryIds(),
-			serviceContext.getAssetTagNames());
 
 		return group;
 	}
