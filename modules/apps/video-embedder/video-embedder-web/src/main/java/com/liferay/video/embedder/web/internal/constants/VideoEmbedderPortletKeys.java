49f9d5d92de65075423a3e7569a6f6f0eb210be3
Jürgen Kappler <jurgen.kappler@liferay.com>
2017-11-08 12:34:54 +0100

    LPS-28940 Update portlet keys

--- modules/apps/video-embedder/video-embedder-web/src/main/java/com/liferay/video/embedder/web/internal/constants/VideoEmbedderPortletKeys.java
+++ modules/apps/video-embedder/video-embedder-web/src/main/java/com/liferay/video/embedder/web/internal/constants/VideoEmbedderPortletKeys.java
@@ -21,6 +21,6 @@ package com.liferay.video.embedder.web.internal.constants;
 public class VideoEmbedderPortletKeys {
 
 	public static final String VideoEmbedder =
-		"com_liferay_youtube_web_portlet_VideoEmbedderPortlet";
+		"com_liferay_video_embedder_web_portlet_VideoEmbedderPortlet";
 
 }
