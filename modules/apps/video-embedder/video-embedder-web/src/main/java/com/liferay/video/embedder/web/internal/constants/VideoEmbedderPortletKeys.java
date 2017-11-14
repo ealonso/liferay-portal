2a48246861433079d273f05e79b96d948108be2c
arthurchan35 <arthurchan35@gmail.com>
2017-11-14 10:25:03 +0100

    LPS-28940 Portlet keys for the module

---
+++ modules/apps/video-embedder/video-embedder-web/src/main/java/com/liferay/video/embedder/web/internal/constants/VideoEmbedderPortletKeys.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.video.embedder.web.internal.constants;
+
+/**
+ * @author arthurchan35
+ */
+public class VideoEmbedderPortletKeys {
+
+	public static final String VideoEmbedder =
+		"com_liferay_video_embedder_web_portlet_VideoEmbedderPortlet";
+
+}
