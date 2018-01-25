b4a8f5032873f09d08f5ea08b802b1ceb88f7d2e
Alessio Rendina <asilcomando@gmail.com>
2018-01-25 18:07:08 +0100

    LPS-77353 Added group creation step base implementation

---
+++ modules/apps/web-experience/site/site-api/src/main/java/com/liferay/site/util/BaseGroupCreationStep.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.util;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public abstract class BaseGroupCreationStep implements GroupCreationStep {
+
+	@Override
+	public String getLabel(HttpServletRequest httpServletRequest) {
+		return LanguageUtil.get(httpServletRequest, getName());
+	}
+
+	@Override
+	public boolean isActive(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+
+		return true;
+	}
+
+	@Override
+	public boolean isSennaDisabled() {
+		return false;
+	}
+
+	@Override
+	public boolean isVisible(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+
+		return isActive(httpServletRequest, httpServletResponse);
+	}
+
+	@Override
+	public boolean showControls(
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse) {
+
+		return true;
+	}
+
+}
