bcee51941e6ec4b5bf01a7059e693e758b9390c9
Alessio Rendina <asilcomando@gmail.com>
2018-01-25 18:03:48 +0100

    LPS-77353 Added group starter kit and registry interface

---
+++ modules/apps/web-experience/site/site-api/src/main/java/com/liferay/site/util/GroupStarterKit.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.util;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public interface GroupStarterKit {
+
+	public void create(HttpServletRequest httpServletRequest) throws Exception;
+
+	public String getDescription(Locale locale);
+
+	public String getKey();
+
+	public String getName(Locale locale);
+
+	public String getThumbnailSrc();
+
+	public boolean isActive(HttpServletRequest httpServletRequest);
+
+	public void renderPreview(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception;
+
+}
