c7fc91e87d2340e24ce05e73dee800e1e68a75c5
pavel-savinov <pavel.savinov@liferay.com>
2018-03-06 12:40:02 +0100

    LPS-77244 Use type settings to store add-to-the-primary-navigation flag

--- modules/apps/web-experience/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/AddContentLayoutMVCActionCommand.java
+++ modules/apps/web-experience/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/AddContentLayoutMVCActionCommand.java
@@ -32,6 +32,7 @@ import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PropertiesParamUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.WebKeys;
 
@@ -78,14 +79,9 @@ public class AddContentLayoutMVCActionCommand extends BaseMVCActionCommand {
 
 		nameMap.put(themeDisplay.getLocale(), name);
 
-		long layoutPageTemplateEntryId = ParamUtil.getLong(
-			actionRequest, "layoutPageTemplateEntryId");
-
-		UnicodeProperties typeSettingsProperties = new UnicodeProperties();
-
-		typeSettingsProperties.setProperty(
-			"layoutPageTemplateEntryId",
-			String.valueOf(layoutPageTemplateEntryId));
+		UnicodeProperties typeSettingsProperties =
+			PropertiesParamUtil.getProperties(
+				actionRequest, "TypeSettingsProperties--");
 
 		ServiceContext serviceContext = ServiceContextFactory.getInstance(
 			Layout.class.getName(), actionRequest);
