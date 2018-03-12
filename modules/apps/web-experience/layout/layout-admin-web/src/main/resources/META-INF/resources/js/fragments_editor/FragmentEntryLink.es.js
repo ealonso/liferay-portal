74d5137964be92529aa8e20670e1d6a63cbbedcd
pavel-savinov <pavel.savinov@liferay.com>
2018-03-12 11:22:28 +0100

    LPS-78687 Add showControlToolbar property to be able to hide control toolbar in the fragment display portlet

--- modules/apps/web-experience/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/FragmentEntryLink.es.js
+++ modules/apps/web-experience/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/FragmentEntryLink.es.js
@@ -348,6 +348,17 @@ FragmentEntryLink.STATE = {
 	name: Config.string().value(''),
 
 	/**
+	 * Shows FragmentEntryLink control toolbar
+	 * @default true
+	 * @instance
+	 * @memberOf FragmentEntryLink
+	 * @review
+	 * @type {!bool}
+	 */
+
+	showControlBar: Config.bool().value(true),
+
+	/**
 	 * Fragment spritemap
 	 * @default undefined
 	 * @instance
