48d6c21d73d813504894400d7a945a1a7aac1f05
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-04-02 12:54:08 +0100

    LPS-77931 Propagate imageSelectorURL

--- modules/apps/web-experience/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/FragmentsEditor.es.js
+++ modules/apps/web-experience/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/FragmentsEditor.es.js
@@ -511,6 +511,17 @@ FragmentsEditor.STATE = {
 	id: Config.string().value(''),
 
 	/**
+	 * Image selector url
+	 * @default undefined
+	 * @instance
+	 * @memberOf FragmentsEditor
+	 * @review
+	 * @type {!string}
+	 */
+
+	imageSelectorURL: Config.string().required(),
+
+	/**
 	 * List of fragment instances being used, the order
 	 * of the elements in this array defines their position.
 	 * @default []
