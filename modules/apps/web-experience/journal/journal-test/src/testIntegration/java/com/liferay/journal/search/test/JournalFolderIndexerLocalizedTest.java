2ace5d1f64726da13f1ff518eec4f8698cccc283
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-04-11 16:29:34 +0200

    LPS-79338 Uses IndexerRegistry to get the indexer

--- modules/apps/web-experience/journal/journal-test/src/testIntegration/java/com/liferay/journal/search/test/JournalFolderIndexerLocalizedTest.java
+++ modules/apps/web-experience/journal/journal-test/src/testIntegration/java/com/liferay/journal/search/test/JournalFolderIndexerLocalizedTest.java
@@ -15,14 +15,15 @@
 package com.liferay.journal.search.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.journal.model.JournalFolder;
 import com.liferay.journal.model.JournalFolderConstants;
-import com.liferay.journal.search.JournalFolderIndexer;
 import com.liferay.journal.test.util.JournalTestUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Hits;
 import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistry;
 import com.liferay.portal.kernel.search.QueryConfig;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
@@ -36,6 +37,7 @@ import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.service.test.ServiceTestUtil;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import java.util.ArrayList;
@@ -65,11 +67,11 @@ public class JournalFolderIndexerLocalizedTest {
 	public void setUp() throws Exception {
 		_group = GroupTestUtil.addGroup();
 
+		_indexer = _indexerRegistry.getIndexer(JournalFolder.class);
+
 		ServiceTestUtil.setUser(TestPropsValues.getUser());
 
 		CompanyThreadLocal.setCompanyId(TestPropsValues.getCompanyId());
-
-		_indexer = new JournalFolderIndexer();
 	}
 
 	@Test
@@ -200,9 +202,12 @@ public class JournalFolderIndexerLocalizedTest {
 		}
 	}
 
+	@Inject
+	private static IndexerRegistry _indexerRegistry;
+
 	@DeleteAfterTestRun
 	private Group _group;
 
-	private Indexer<?> _indexer;
+	private Indexer<JournalFolder> _indexer;
 
 }
