5791f4ded1c91fc26582884add7ee2f8d64f2f09
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-04-11 15:37:22 +0200

    LPS-79338 Rename to folder

--- modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalFolderIndexer.java
+++ modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalFolderIndexer.java
@@ -109,27 +109,24 @@ public class JournalFolderIndexer
 	}
 
 	@Override
-	protected void doDelete(JournalFolder journalFolder) throws Exception {
-		deleteDocument(
-			journalFolder.getCompanyId(), journalFolder.getFolderId());
+	protected void doDelete(JournalFolder folder) throws Exception {
+		deleteDocument(folder.getCompanyId(), folder.getFolderId());
 	}
 
 	@Override
-	protected Document doGetDocument(JournalFolder journalFolder)
-		throws Exception {
-
+	protected Document doGetDocument(JournalFolder folder) throws Exception {
 		if (_log.isDebugEnabled()) {
-			_log.debug("Indexing journalFolder " + journalFolder);
+			_log.debug("Indexing folder " + folder);
 		}
 
-		Document document = getBaseModelDocument(CLASS_NAME, journalFolder);
+		Document document = getBaseModelDocument(CLASS_NAME, folder);
 
 		String[] languageIds = LocaleUtil.toLanguageIds(
 			LanguageUtil.getSupportedLocales());
 
-		String title = journalFolder.getName();
+		String title = folder.getName();
 
-		if (journalFolder.isInTrash()) {
+		if (folder.isInTrash()) {
 			title = _trashHelper.getOriginalTitle(title);
 		}
 
@@ -137,19 +134,19 @@ public class JournalFolderIndexer
 			document.addText(
 				LocalizationUtil.getLocalizedName(
 					Field.DESCRIPTION, languageId),
-				journalFolder.getDescription());
+				folder.getDescription());
 			document.addText(
 				LocalizationUtil.getLocalizedName(Field.TITLE, languageId),
 				title);
 		}
 
-		document.addKeyword(Field.FOLDER_ID, journalFolder.getParentFolderId());
+		document.addKeyword(Field.FOLDER_ID, folder.getParentFolderId());
 		document.addKeyword(
 			Field.TREE_PATH,
-			StringUtil.split(journalFolder.getTreePath(), CharPool.SLASH));
+			StringUtil.split(folder.getTreePath(), CharPool.SLASH));
 
 		if (_log.isDebugEnabled()) {
-			_log.debug("Document " + journalFolder + " indexed successfully");
+			_log.debug("Document " + folder + " indexed successfully");
 		}
 
 		return document;
@@ -169,11 +166,11 @@ public class JournalFolderIndexer
 	}
 
 	@Override
-	protected void doReindex(JournalFolder journalFolder) throws Exception {
-		Document document = getDocument(journalFolder);
+	protected void doReindex(JournalFolder folder) throws Exception {
+		Document document = getDocument(folder);
 
 		_indexWriterHelper.updateDocument(
-			getSearchEngineId(), journalFolder.getCompanyId(), document,
+			getSearchEngineId(), folder.getCompanyId(), document,
 			isCommitImmediately());
 	}
 
