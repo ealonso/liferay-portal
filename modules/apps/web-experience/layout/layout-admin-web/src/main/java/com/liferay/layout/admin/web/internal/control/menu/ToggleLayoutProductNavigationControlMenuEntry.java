94fe8a3910e0dded8aa5ff006e4865b5d4473d0f
pavel-savinov <pavel.savinov@liferay.com>
2018-04-25 16:23:46 +0200

    LPS-80145 Layout mode toggle icon

--- modules/apps/web-experience/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/control/menu/ToggleLayoutProductNavigationControlMenuEntry.java
+++ modules/apps/web-experience/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/control/menu/ToggleLayoutProductNavigationControlMenuEntry.java
@@ -15,22 +15,20 @@
 package com.liferay.layout.admin.web.internal.control.menu;
 
 import com.liferay.layout.admin.constants.LayoutAdminPortletKeys;
-import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.theme.PortletDisplay;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Html;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.product.navigation.control.menu.BaseProductNavigationControlMenuEntry;
 import com.liferay.product.navigation.control.menu.ProductNavigationControlMenuEntry;
 import com.liferay.product.navigation.control.menu.constants.ProductNavigationControlMenuCategoryKeys;
-import com.liferay.taglib.aui.IconTag;
-import com.liferay.taglib.servlet.PageContextFactoryUtil;
-import com.liferay.taglib.ui.SuccessTag;
 
 import java.io.IOException;
 import java.io.Writer;
@@ -38,15 +36,10 @@ import java.io.Writer;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
-import java.util.ResourceBundle;
-
-import javax.portlet.PortletRequest;
-import javax.portlet.PortletURL;
+import java.util.Objects;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.PageContext;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -58,11 +51,14 @@ import org.osgi.service.component.annotations.Reference;
 	immediate = true,
 	property = {
 		"product.navigation.control.menu.category.key=" + ProductNavigationControlMenuCategoryKeys.USER,
-		"product.navigation.control.menu.entry.order:Integer=100"
+		"product.navigation.control.menu.entry.order:Integer=50"
 	},
-	service = ProductNavigationControlMenuEntry.class
+	service = {
+		ProductNavigationControlMenuEntry.class,
+		ToggleLayoutProductNavigationControlMenuEntry.class
+	}
 )
-public class ManageLayoutProductNavigationControlMenuEntry
+public class ToggleLayoutProductNavigationControlMenuEntry
 	extends BaseProductNavigationControlMenuEntry {
 
 	@Override
@@ -80,60 +76,44 @@ public class ManageLayoutProductNavigationControlMenuEntry
 			HttpServletRequest request, HttpServletResponse response)
 		throws IOException {
 
-		Map<String, String> values = new HashMap<>();
-
 		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
-		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
-			"content.Language", themeDisplay.getLocale(), getClass());
+		Map<String, String> values = new HashMap<>();
 
 		values.put(
-			"configurePage",
-			_html.escape(_language.get(resourceBundle, "configure-page")));
+			"iconEdit",
+			themeDisplay.getPathThemeImages() + "/lexicon/icons.svg#cog");
+		values.put(
+			"iconView",
+			themeDisplay.getPathThemeImages() + "/lexicon/icons.svg#view");
+		values.put(
+			"namespace",
+			_portal.getPortletNamespace(LayoutAdminPortletKeys.GROUP_PAGES));
 
-		PortletURL editPageURL = _portal.getControlPanelPortletURL(
-			request, LayoutAdminPortletKeys.GROUP_PAGES,
-			PortletRequest.RENDER_PHASE);
+		boolean editMode = ParamUtil.getBoolean(request, "p_p_edit");
 
-		Layout layout = themeDisplay.getLayout();
+		String redirect = themeDisplay.getURLCurrent();
 
-		editPageURL.setParameter("mvcRenderCommandName", "/layout/edit_layout");
-		editPageURL.setParameter("backURL", _portal.getCurrentURL(request));
-		editPageURL.setParameter(
-			"groupId", String.valueOf(layout.getGroupId()));
-		editPageURL.setParameter("selPlid", String.valueOf(layout.getPlid()));
-		editPageURL.setParameter(
-			"privateLayout", String.valueOf(layout.isPrivateLayout()));
+		if (editMode) {
+			try {
+				long selPlid = ParamUtil.getLong(request, "p_r_p_selPlid");
 
-		values.put("editPageURL", editPageURL.toString());
+				if (selPlid > 0) {
+					Layout layout = _layoutLocalService.getLayout(
+						ParamUtil.getLong(request, "p_r_p_selPlid"));
 
-		try {
-			IconTag iconTag = new IconTag();
-
-			iconTag.setCssClass("icon-monospaced");
-			iconTag.setImage("cog");
-			iconTag.setMarkupView("lexicon");
-
-			PageContext pageContext = PageContextFactoryUtil.create(
-				request, response);
-
-			values.put("iconCog", iconTag.doTagAsString(pageContext));
-
-			SuccessTag successTag = new SuccessTag();
-
-			successTag.setKey("layoutUpdated");
-			successTag.setMessage(
-				_language.get(
-					resourceBundle, "the-page-was-updated-succesfully"));
-			successTag.setTargetNode("#controlMenuAlertsContainer");
-
-			values.put(
-				"layoutUpdatedMessage", successTag.doTagAsString(pageContext));
+					redirect = _portal.getLayoutFullURL(layout, themeDisplay);
+				}
+			}
+			catch (PortalException pe) {
+				throw new IOException(pe);
+			}
 		}
-		catch (JspException je) {
-			ReflectionUtil.throwException(je);
-		}
+
+		values.put("checked", editMode ? "checked" : "");
+		values.put("editMode", String.valueOf(!editMode));
+		values.put("redirect", redirect);
 
 		Writer writer = response.getWriter();
 
@@ -146,6 +126,10 @@ public class ManageLayoutProductNavigationControlMenuEntry
 	public boolean isShow(HttpServletRequest request) throws PortalException {
 		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
 			WebKeys.THEME_DISPLAY);
+
+		if (_isConfiguration(request, themeDisplay)) {
+			return true;
+		}
 
 		Layout layout = themeDisplay.getLayout();
 
@@ -162,16 +146,34 @@ public class ManageLayoutProductNavigationControlMenuEntry
 		return super.isShow(request);
 	}
 
+	private boolean _isConfiguration(
+		HttpServletRequest request, ThemeDisplay themeDisplay) {
+
+		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+		String path = ParamUtil.getString(
+			request, portletDisplay.getNamespace() + "mvcRenderCommandName");
+
+		if (Objects.equals(path, "/layout/edit_layout")) {
+			return true;
+		}
+
+		return false;
+	}
+
 	private static final String _TMPL_CONTENT = StringUtil.read(
-		ManageLayoutProductNavigationControlMenuEntry.class,
-		"/META-INF/resources/control/menu/edit_layout_control_menu_entry_" +
-			"icon.tmpl");
+		ToggleLayoutProductNavigationControlMenuEntry.class,
+		"/META-INF/resources/control/menu/configure_layout_control_menu_" +
+			"entry_icon.tmpl");
 
 	@Reference
 	private Html _html;
 
 	@Reference
 	private Language _language;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
 
 	@Reference
 	private Portal _portal;
