c07b4ac2b3caa393f6e72e08c9893df6c6f96332
Sandro Chinea <sandrochinea@gmail.com>
2019-01-22 13:57:35 +0100

    LPS-88722 Added config key, panel and templates for Background Color

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/floating_toolbar/FloatingToolbar.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/floating_toolbar/FloatingToolbar.es.js
@@ -2,6 +2,7 @@ import Component from 'metal-component';
 import {Config} from 'metal-state';
 import Soy from 'metal-soy';
 
+import './background_color/FloatingToolbarBackgroundColorPanel.es';
 import './background_image/FloatingToolbarBackgroundImagePanel.es';
 import './spacing/FloatingToolbarSpacingPanel.es';
 import getConnectedComponent from '../../store/ConnectedComponent.es';
@@ -13,6 +14,11 @@ import templates from './FloatingToolbar.soy';
  * @type {object[]}
  */
 const FLOATING_TOOLBAR_PANELS = [
+	{
+		icon: 'format',
+		panelId: 'background_color',
+		title: Liferay.Language.get('background-color')
+	},
 	{
 		icon: 'table',
 		panelId: 'spacing',
