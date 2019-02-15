cbc670470924a30fe26fd783507e98b18932f963
Victor Galan <victor.galan@liferay.com>
2019-02-15 11:55:11 +0100

    LPS-90654 Index localized title in categories

--- modules/apps/asset/asset-categories-service/src/main/java/com/liferay/asset/categories/internal/search/spi/model/index/contributor/AssetVocabularyModelDocumentContributor.java
+++ modules/apps/asset/asset-categories-service/src/main/java/com/liferay/asset/categories/internal/search/spi/model/index/contributor/AssetVocabularyModelDocumentContributor.java
@@ -17,13 +17,17 @@ package com.liferay.asset.categories.internal.search.spi.model.index.contributor
 import com.liferay.asset.kernel.model.AssetVocabulary;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.search.localization.SearchLocalizationHelper;
 import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
 
 import java.util.Locale;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -54,6 +58,11 @@ public class AssetVocabularyModelDocumentContributor
 
 		document.addText(Field.NAME, assetVocabulary.getName());
 
+		document.addLocalizedKeyword(
+			"localized_title",
+			populateMap(assetVocabulary, assetVocabulary.getTitleMap()), true,
+			true);
+
 		_searchLocalizationHelper.addLocalizedField(
 			document, Field.TITLE, siteDefaultLocale,
 			assetVocabulary.getTitleMap());
@@ -66,6 +75,26 @@ public class AssetVocabularyModelDocumentContributor
 		catch (PortalException pe) {
 			throw new SystemException(pe);
 		}
+	}
+
+	protected Map<Locale, String> populateMap(
+		AssetVocabulary assetVocabulary, Map<Locale, String> map) {
+
+		String defaultValue = map.get(
+			LocaleUtil.fromLanguageId(assetVocabulary.getDefaultLanguageId()));
+
+		for (Locale availableLocale :
+				LanguageUtil.getAvailableLocales(
+					assetVocabulary.getGroupId())) {
+
+			if (!map.containsKey(availableLocale) ||
+				Validator.isNull(map.get(availableLocale))) {
+
+				map.put(availableLocale, defaultValue);
+			}
+		}
+
+		return map;
 	}
 
 	@Reference
