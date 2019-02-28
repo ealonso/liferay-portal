b037a66ad90e1d5566c97978034c030b091c2246
Carlos Lancha <carlos.lancha@liferay.com>
2019-02-28 16:19:07 +0100

    LPS-91219 - Create text properties toolbar

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/utils/constants.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/utils/constants.js
@@ -45,3 +45,52 @@ export const FRAGMENTS_EDITOR_ITEM_TYPES = {
 	fragmentList: 'fragments-editor-fragment-list',
 	section: 'fragments-editor-section'
 };
+
+/**
+ * Available editable field config keys
+ */
+export const EDITABLE_FIELD_CONFIG_KEYS = {
+	textAlignment: 'textAlignment',
+	textColor: 'textColor',
+	textStyle: 'textStyle'
+};
+
+/**
+ * Available values for textStyle config key
+ */
+export const TEXT_STYLES = [
+	{
+		textStyleId: 'small',
+		label: Liferay.Language.get('small')
+	},
+	{
+		textStyleId: 'regular',
+		label: Liferay.Language.get('regular')
+	},
+	{
+		textStyleId: 'large',
+		label: Liferay.Language.get('large')
+	}
+];
+
+/**
+ * Available values for textAlignmentOptions config key
+ */
+export const TEXT_ALIGNMENT_OPTIONS = [
+	{
+		textAlignmentId: 'center',
+		label: Liferay.Language.get('center')
+	},
+	{
+		textAlignmentId: 'justify',
+		label: Liferay.Language.get('justify')
+	},
+	{
+		textAlignmentId: 'left',
+		label: Liferay.Language.get('left')
+	},
+	{
+		textAlignmentId: 'right',
+		label: Liferay.Language.get('right')
+	}
+];
