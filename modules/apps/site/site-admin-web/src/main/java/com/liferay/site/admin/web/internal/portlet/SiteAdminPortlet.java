2d6c10d7a04d4623978937b4d2bb3a207374df97
Jonathan McCann <jonathan.mccann@liferay.com>
2019-03-07 13:54:02 -0800

    LPS-91467 There is no need to fetch the Role objects if all we need are the IDs

--- modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portlet/SiteAdminPortlet.java
+++ modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portlet/SiteAdminPortlet.java
@@ -50,7 +50,6 @@ import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.model.LayoutSetPrototype;
 import com.liferay.portal.kernel.model.MembershipRequest;
 import com.liferay.portal.kernel.model.MembershipRequestConstants;
-import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.Team;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
@@ -476,10 +475,10 @@ public class SiteAdminPortlet extends MVCPortlet {
 		return refererGroupId;
 	}
 
-	protected List<Role> getRoles(PortletRequest portletRequest)
+	protected List<Long> getRoleIds(PortletRequest portletRequest)
 		throws Exception {
 
-		List<Role> roles = new ArrayList<>();
+		List<Long> roleIds = new ArrayList<>();
 
 		long[] siteRolesRoleIds = ArrayUtil.unique(
 			ParamUtil.getLongValues(
@@ -490,12 +489,10 @@ public class SiteAdminPortlet extends MVCPortlet {
 				continue;
 			}
 
-			Role role = roleLocalService.getRole(siteRolesRoleId);
-
-			roles.add(role);
+			roleIds.add(siteRolesRoleId);
 		}
 
-		return roles;
+		return roleIds;
 	}
 
 	protected PortletURL getSiteAdministrationURL(
@@ -806,9 +803,7 @@ public class SiteAdminPortlet extends MVCPortlet {
 
 		typeSettingsProperties.setProperty(
 			"defaultSiteRoleIds",
-			ListUtil.toString(
-				getRoles(actionRequest), Role.ROLE_ID_ACCESSOR,
-				StringPool.COMMA));
+			ListUtil.toString(getRoleIds(actionRequest), StringPool.BLANK));
 		typeSettingsProperties.setProperty(
 			"defaultTeamIds",
 			ListUtil.toString(
