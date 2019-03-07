26c91275b977e2380db1b8c3dde85050c99b4b75
Jonathan McCann <jonathan.mccann@liferay.com>
2019-03-07 13:58:05 -0800

    LPS-91467 Apply the same fix for Teams

--- modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portlet/SiteAdminPortlet.java
+++ modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portlet/SiteAdminPortlet.java
@@ -50,7 +50,6 @@ import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.model.LayoutSetPrototype;
 import com.liferay.portal.kernel.model.MembershipRequest;
 import com.liferay.portal.kernel.model.MembershipRequestConstants;
-import com.liferay.portal.kernel.model.Team;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.security.auth.AuthException;
@@ -510,10 +509,10 @@ public class SiteAdminPortlet extends MVCPortlet {
 			actionRequest, group, portletId, 0, 0, PortletRequest.RENDER_PHASE);
 	}
 
-	protected List<Team> getTeams(PortletRequest portletRequest)
+	protected List<Long> getTeamIds(PortletRequest portletRequest)
 		throws Exception {
 
-		List<Team> teams = new ArrayList<>();
+		List<Long> teamIds = new ArrayList<>();
 
 		long[] teamsTeamIds = ArrayUtil.unique(
 			ParamUtil.getLongValues(
@@ -524,12 +523,10 @@ public class SiteAdminPortlet extends MVCPortlet {
 				continue;
 			}
 
-			Team team = teamLocalService.getTeam(teamsTeamId);
-
-			teams.add(team);
+			teamIds.add(teamsTeamId);
 		}
 
-		return teams;
+		return teamIds;
 	}
 
 	@Override
@@ -806,9 +803,7 @@ public class SiteAdminPortlet extends MVCPortlet {
 			ListUtil.toString(getRoleIds(actionRequest), StringPool.BLANK));
 		typeSettingsProperties.setProperty(
 			"defaultTeamIds",
-			ListUtil.toString(
-				getTeams(actionRequest), Team.TEAM_ID_ACCESSOR,
-				StringPool.COMMA));
+			ListUtil.toString(getTeamIds(actionRequest), StringPool.BLANK));
 
 		String[] analyticsTypes = PrefsPropsUtil.getStringArray(
 			themeDisplay.getCompanyId(), PropsKeys.ADMIN_ANALYTICS_TYPES,
