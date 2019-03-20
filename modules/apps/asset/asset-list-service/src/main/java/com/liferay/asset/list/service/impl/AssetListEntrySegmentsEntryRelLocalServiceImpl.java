1510c92f00ec2767ade8bc8c145ca35989811ad2
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-03-20 10:52:42 +0100

    LPS-91996 Uses parameters instead of service context

--- modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/service/impl/AssetListEntrySegmentsEntryRelLocalServiceImpl.java
+++ modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/service/impl/AssetListEntrySegmentsEntryRelLocalServiceImpl.java
@@ -35,11 +35,12 @@ public class AssetListEntrySegmentsEntryRelLocalServiceImpl
 
 	@Override
 	public AssetListEntrySegmentsEntryRel addAssetListEntrySegmentsEntryRel(
-			long assetListEntryId, long segmentsEntryId, String typeSettings,
+			long userId, long groupId, long assetListEntryId,
+			long segmentsEntryId, String typeSettings,
 			ServiceContext serviceContext)
 		throws PortalException {
 
-		User user = userLocalService.getUser(serviceContext.getUserId());
+		User user = userLocalService.getUser(userId);
 
 		long assetListEntrySegmentsEntryRelId = counterLocalService.increment();
 
@@ -48,16 +49,12 @@ public class AssetListEntrySegmentsEntryRelLocalServiceImpl
 				assetListEntrySegmentsEntryRelId);
 
 		assetListEntrySegmentsEntryRel.setUuid(serviceContext.getUuid());
-		assetListEntrySegmentsEntryRel.setGroupId(
-			serviceContext.getScopeGroupId());
-		assetListEntrySegmentsEntryRel.setCompanyId(
-			serviceContext.getCompanyId());
-		assetListEntrySegmentsEntryRel.setUserId(serviceContext.getUserId());
+		assetListEntrySegmentsEntryRel.setGroupId(groupId);
+		assetListEntrySegmentsEntryRel.setCompanyId(user.getCompanyId());
+		assetListEntrySegmentsEntryRel.setUserId(userId);
 		assetListEntrySegmentsEntryRel.setUserName(user.getFullName());
-		assetListEntrySegmentsEntryRel.setCreateDate(
-			serviceContext.getCreateDate(new Date()));
-		assetListEntrySegmentsEntryRel.setModifiedDate(
-			serviceContext.getModifiedDate(new Date()));
+		assetListEntrySegmentsEntryRel.setCreateDate(new Date());
+		assetListEntrySegmentsEntryRel.setModifiedDate(new Date());
 		assetListEntrySegmentsEntryRel.setAssetListEntryId(assetListEntryId);
 		assetListEntrySegmentsEntryRel.setSegmentsEntryId(segmentsEntryId);
 		assetListEntrySegmentsEntryRel.setTypeSettings(typeSettings);
@@ -117,10 +114,8 @@ public class AssetListEntrySegmentsEntryRelLocalServiceImpl
 
 	@Override
 	public AssetListEntrySegmentsEntryRel
-			updateAssetListEntrySegmentsEntryRelTypeSettings(
-				long assetListEntryId, long segmentsEntryId,
-				String typeSettings)
-		throws PortalException {
+		updateAssetListEntrySegmentsEntryRelTypeSettings(
+			long assetListEntryId, long segmentsEntryId, String typeSettings) {
 
 		AssetListEntrySegmentsEntryRel assetListEntrySegmentsEntryRel =
 			assetListEntrySegmentsEntryRelPersistence.fetchByA_S(
@@ -135,10 +130,9 @@ public class AssetListEntrySegmentsEntryRelLocalServiceImpl
 
 	@Override
 	public AssetListEntrySegmentsEntryRel
-			updateAssetListEntrySegmentsEntryRelTypeSettingsProperties(
-				long assetListEntryId, long segmentsEntryId,
-				String typeSettingsProperties)
-		throws PortalException {
+		updateAssetListEntrySegmentsEntryRelTypeSettingsProperties(
+			long assetListEntryId, long segmentsEntryId,
+			String typeSettingsProperties) {
 
 		AssetListEntrySegmentsEntryRel assetListEntrySegmentsEntryRel =
 			assetListEntrySegmentsEntryRelPersistence.fetchByA_S(
