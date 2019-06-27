93171f143f059ed5da3fe8b32d621c8b9847097f
rubenpulido <ruben.pulido@liferay.com>
2019-06-27 11:59:11 +0200

    LPS-96691 Call getDefaultEditablesValuesJSONObject(FragementEntryLink) instead of deprecated method

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryLinkLocalServiceImpl.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryLinkLocalServiceImpl.java
@@ -187,7 +187,7 @@ public class FragmentEntryLinkLocalServiceImpl
 		if (Validator.isNull(editableValues)) {
 			JSONObject jsonObject =
 				_fragmentEntryProcessorRegistry.
-					getDefaultEditableValuesJSONObject(html);
+					getDefaultEditableValuesJSONObject(fragmentEntryLink);
 
 			editableValues = jsonObject.toString();
 		}
