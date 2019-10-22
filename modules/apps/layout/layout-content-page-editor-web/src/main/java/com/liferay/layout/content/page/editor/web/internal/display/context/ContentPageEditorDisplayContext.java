be8041f4a36b31bc6554454e52df3c8a29c7a35b
Victor Galan <victor.galan@liferay.com>
2019-10-22 08:49:38 +0200

    LPS-103460 Include the fragment entry key in the display context, it is used to determine the drop zone

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -908,6 +908,8 @@ public class ContentPageEditorDisplayContext {
 			soyContext.put(
 				"fragmentEntryId", fragmentEntry.getFragmentEntryId()
 			).put(
+				"fragmentEntryKey", fragmentEntry.getFragmentEntryKey()
+			).put(
 				"name", fragmentEntry.getName()
 			);
 
@@ -933,6 +935,8 @@ public class ContentPageEditorDisplayContext {
 			if (fragmentRenderer != null) {
 				soyContext.put(
 					"fragmentEntryId", 0
+				).put(
+					"fragmentEntryKey", fragmentRenderer.getKey()
 				).put(
 					"name", fragmentRenderer.getLabel(themeDisplay.getLocale())
 				);
