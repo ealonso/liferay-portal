92e816be606705b12a8855b13335681c8d00aa91
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-12-16 11:28:48 +0100

    Update Tests

--- modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/search/test/JournalArticleSearchTest.java
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/search/test/JournalArticleSearchTest.java
@@ -522,12 +522,6 @@ public class JournalArticleSearchTest extends BaseSearchTestCase {
 	}
 
 	@Override
-	protected boolean isCheckBaseModelPermission() {
-		return _journalServiceConfiguration.
-			articleViewPermissionsCheckEnabled();
-	}
-
-	@Override
 	protected boolean isExpirableAllVersions() {
 		return _journalServiceConfiguration.expireAllArticleVersionsEnabled();
 	}
--- modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/facet/faceted/searcher/test/PermissionFilterFacetedSearcherTest.java
+++ modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/facet/faceted/searcher/test/PermissionFilterFacetedSearcherTest.java
@@ -16,7 +16,6 @@ package com.liferay.portal.search.facet.faceted.searcher.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.dynamic.data.mapping.test.util.DDMStructureTestUtil;
-import com.liferay.journal.configuration.JournalServiceConfiguration;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.model.JournalFolder;
 import com.liferay.journal.service.JournalArticleLocalService;
@@ -38,7 +37,6 @@ import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
-import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.search.facet.type.AssetEntriesFacetFactory;
 import com.liferay.portal.search.test.util.FacetsAssert;
@@ -47,7 +45,6 @@ import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Dictionary;
 import java.util.List;
 import java.util.Map;
 
@@ -77,8 +74,6 @@ public class PermissionFilterFacetedSearcherTest
 	@Override
 	public void setUp() throws Exception {
 		super.setUp();
-
-		setUpJournalServiceConfiguration();
 
 		_originalPermissionChecker =
 			PermissionThreadLocal.getPermissionChecker();
@@ -175,17 +170,6 @@ public class PermissionFilterFacetedSearcherTest
 		serviceContext.setModelPermissions(modelPermissions);
 
 		return serviceContext;
-	}
-
-	protected void setUpJournalServiceConfiguration() throws Exception {
-		_configuration = configurationAdmin.getConfiguration(
-			JournalServiceConfiguration.class.getName(), StringPool.QUESTION);
-
-		Dictionary<String, Object> properties = new HashMapDictionary<>();
-
-		properties.put("articleViewPermissionsCheckEnabled", true);
-
-		_configuration.update(properties);
 	}
 
 	protected void tearDownJournalServiceConfiguration() throws Exception {
