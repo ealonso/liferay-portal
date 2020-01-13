baa25e2cb15f406cfa2a8adfb919701a8826abfd
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-01-13 08:54:43 +0100

    wip

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/master_layout/AllowedFragmentSelector.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/master_layout/AllowedFragmentSelector.es.js
@@ -98,6 +98,18 @@ const AllowedFragmentSelector = ({onSelectedFragment}) => {
 
 	const fragmentEntryKeysArray = toFragmentEntryKeysArray(elements);
 
+	const [allowNewFragmentEntries, setAllowNewFragmentEntries] = useState(
+		initialAllowNewFragmentEntries
+	);
+
+	const [fragmentEntryKeys, setFragmentEntryKeys] = useState(
+		getSelectedNodeIds(
+			allowNewFragmentEntries,
+			initialFragmentEntryKeys,
+			fragmentEntryKeysArray
+		)
+	);
+
 	return (
 		<>
 			<ClayInput
@@ -111,34 +123,50 @@ const AllowedFragmentSelector = ({onSelectedFragment}) => {
 			<Treeview
 				filterQuery={filter}
 				inheritSelection
-				initialSelectedNodeIds={getSelectedNodeIds(
-					initialAllowNewFragmentEntries,
-					initialFragmentEntryKeys,
-					fragmentEntryKeysArray
-				)}
+				initialSelectedNodeIds={fragmentEntryKeys}
 				NodeComponent={AllowedFragmentTreeNode}
 				nodes={nodes}
-				onSelectedNodesChange={selectedNodeIds =>
+				onSelectedNodesChange={selectedNodeIds => {
+					const newFragmentEntryKeys = getSelectedNodeIds(
+						allowNewFragmentEntries,
+						Array.from(selectedNodeIds),
+						fragmentEntryKeysArray
+					);
+
+					setFragmentEntryKeys(newFragmentEntryKeys);
+
 					onSelectedFragment(
-						getSelectedNodeIds(
-							initialAllowNewFragmentEntries,
-							Array.from(selectedNodeIds),
-							fragmentEntryKeysArray
-						)
-					)
-				}
+						allowNewFragmentEntries,
+						newFragmentEntryKeys
+					);
+				}}
 			/>
 
 			<ClayCheckbox
 				aria-label={Liferay.Language.get(
 					'make-allowed-all-new-fragments-created'
 				)}
-				checked={initialAllowNewFragmentEntries}
+				checked={allowNewFragmentEntries}
 				label={Liferay.Language.get(
 					'make-allowed-all-new-fragments-created'
 				)}
-				onChange={() => {
-					//
+				onChange={event => {
+					const newAllowNewFragmentEntries = event.target.checked;
+
+					setAllowNewFragmentEntries(newAllowNewFragmentEntries);
+
+					const newFragmentEntryKeys = getSelectedNodeIds(
+						newAllowNewFragmentEntries,
+						fragmentEntryKeys,
+						fragmentEntryKeysArray
+					);
+
+					setFragmentEntryKeys(newFragmentEntryKeys);
+
+					onSelectedFragment(
+						newAllowNewFragmentEntries,
+						newFragmentEntryKeys
+					);
 				}}
 			/>
 		</>
