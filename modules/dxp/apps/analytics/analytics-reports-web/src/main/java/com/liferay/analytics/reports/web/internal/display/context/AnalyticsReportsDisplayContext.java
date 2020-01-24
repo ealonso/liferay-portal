8fd7df16294fad82680e5a0f1ab5944a58bb0098
David Arques <davidarchena@gmail.com>
2020-01-24 12:15:57 +0100

    LPS-106620 Inject content performance props from Page: authorName, publishDate and title

--- modules/dxp/apps/analytics/analytics-reports-web/src/main/java/com/liferay/analytics/reports/web/internal/display/context/AnalyticsReportsDisplayContext.java
+++ modules/dxp/apps/analytics/analytics-reports-web/src/main/java/com/liferay/analytics/reports/web/internal/display/context/AnalyticsReportsDisplayContext.java
@@ -14,28 +14,51 @@
 
 package com.liferay.analytics.reports.web.internal.display.context;
 
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.FastDateFormatFactoryUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.PrefsPropsUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
+ * @author David Arques
  * @author Sarai Díaz
  */
 public class AnalyticsReportsDisplayContext {
 
-	public AnalyticsReportsDisplayContext() {
+	public AnalyticsReportsDisplayContext(
+		HttpServletRequest httpServletRequest,
+		UserLocalService userLocalService) {
+
+		_httpServletRequest = httpServletRequest;
+
+		_themeDisplay = (ThemeDisplay)_httpServletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		_userLocalService = userLocalService;
 	}
 
-	public Map<String, Object> getData() throws Exception {
+	public Map<String, Object> getData() {
 		if (_data != null) {
 			return _data;
 		}
 
 		_data = HashMapBuilder.<String, Object>put(
-			"context", ""
+			"context", StringPool.BLANK
 		).put(
-			"props", ""
+			"props",
+			getProps(_themeDisplay.getLayout(), _themeDisplay.getLocale())
 		).build();
 
 		return _data;
@@ -45,6 +68,44 @@ public class AnalyticsReportsDisplayContext {
 		return PrefsPropsUtil.getString(companyId, "liferayAnalyticsURL");
 	}
 
+	protected Map<String, Object> getProps(Layout layout, Locale locale) {
+		return _getProps(layout, locale);
+	}
+
+	private HashMap<String, Object> _createProps(
+		String authorName, Locale locale, Date publishDate, String title) {
+
+		return HashMapBuilder.<String, Object>put(
+			"authorName", authorName
+		).put(
+			"publishDate",
+			FastDateFormatFactoryUtil.getSimpleDateFormat(
+				"MMMM dd, yyyy", locale
+			).format(
+				publishDate
+			)
+		).put(
+			"title", title
+		).build();
+	}
+
+	private Map<String, Object> _getProps(Layout layout, Locale locale) {
+		String authorName = StringPool.BLANK;
+
+		User user = _userLocalService.fetchUser(layout.getUserId());
+
+		if (user != null) {
+			authorName = user.getFullName();
+		}
+
+		return _createProps(
+			authorName, locale, layout.getPublishDate(),
+			layout.getTitle(locale));
+	}
+
 	private Map<String, Object> _data;
+	private final HttpServletRequest _httpServletRequest;
+	private final ThemeDisplay _themeDisplay;
+	private final UserLocalService _userLocalService;
 
 }
