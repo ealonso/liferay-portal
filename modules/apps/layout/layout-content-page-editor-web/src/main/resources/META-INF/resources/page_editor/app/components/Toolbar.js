1fe97a5d34fb1500f172e4db6e406d865d83086c
Victor Galan <victor.galan@liferay.com>
2020-01-29 15:09:19 +0000

    LPS-107678 Show different message when editing a master page

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/Toolbar.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/Toolbar.js
@@ -21,6 +21,7 @@ import useLazy from '../../core/hooks/useLazy';
 import useLoad from '../../core/hooks/useLoad';
 import usePlugins from '../../core/hooks/usePlugins';
 import * as Actions from '../actions/index';
+import {PAGE_TYPES} from '../config/constants/pageTypes';
 import {ConfigContext} from '../config/index';
 import {useSelector, useDispatch} from '../store/index';
 import {useSelectItem} from './Controls';
@@ -49,6 +50,7 @@ function ToolbarBody() {
 		classPK,
 		discardDraftRedirectURL,
 		discardDraftURL,
+		pageType,
 		publishURL,
 		redirectURL,
 		singleSegmentsExperienceMode,
@@ -124,6 +126,7 @@ function ToolbarBody() {
 		}
 	};
 
+	const isMasterLayout = pageType === PAGE_TYPES.master;
 	return (
 		<div
 			className="container-fluid container-fluid-max-xl"
@@ -222,7 +225,9 @@ function ToolbarBody() {
 							small
 							type="submit"
 						>
-							{singleSegmentsExperienceMode
+							{isMasterLayout
+								? Liferay.Language.get('publish-master')
+								: singleSegmentsExperienceMode
 								? Liferay.Language.get('save-variant')
 								: Liferay.Language.get('publish')}
 						</ClayButton>
