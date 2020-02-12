58ef31c6ff7199029dc16e5066b394fa8a042070
Carlos Lancha <carlos.lancha@liferay.com>
2020-02-12 14:58:35 +0100

    LPS-108506 Pass item id to miller columns items

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/miller_columns/MillerColumnsColumn.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/miller_columns/MillerColumnsColumn.es.js
@@ -26,6 +26,7 @@ const MillerColumnsColumn = ({index, items}) => {
 					description={item.description}
 					draggable={index !== 0}
 					hasChild={item.hasChild}
+					id={item.id}
 					key={item.url}
 					selectable={index !== 0}
 					state={item.state}
