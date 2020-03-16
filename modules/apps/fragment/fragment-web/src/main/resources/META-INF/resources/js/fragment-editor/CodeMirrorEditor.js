f40797686777d033fe6521a6a7166a76939d85d1
Carlos Lancha <carlos.lancha@liferay.com>
2020-03-16 20:05:15 +0100

    LPS-110348 SF

--- modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/fragment-editor/CodeMirrorEditor.js
+++ modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/fragment-editor/CodeMirrorEditor.js
@@ -12,16 +12,6 @@
  * details.
  */
 
-import ClayIcon from '@clayui/icon';
-import CodeMirror from 'codemirror';
-import React, {useEffect, useRef} from 'react';
-
-import 'codemirror/addon/hint/show-hint.css';
-
-import 'codemirror/lib/codemirror.css';
-
-import 'codemirror/mode/css/css';
-
 import 'codemirror/addon/display/autorefresh';
 
 import 'codemirror/addon/edit/closebrackets';
@@ -46,19 +36,28 @@ import 'codemirror/addon/fold/xml-fold';
 
 import 'codemirror/addon/hint/css-hint';
 
+import 'codemirror/addon/hint/html-hint';
+
 import 'codemirror/addon/hint/javascript-hint';
+
+import 'codemirror/addon/hint/show-hint.css';
 
 import 'codemirror/addon/hint/show-hint';
 
 import 'codemirror/addon/hint/xml-hint';
 
-import 'codemirror/addon/hint/html-hint';
+import 'codemirror/lib/codemirror.css';
 
-import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/css/css';
+
+import 'codemirror/mode/htmlmixed/htmlmixed';
 
 import 'codemirror/mode/javascript/javascript';
 
-import 'codemirror/mode/htmlmixed/htmlmixed';
+import 'codemirror/mode/xml/xml';
+import ClayIcon from '@clayui/icon';
+import CodeMirror from 'codemirror';
+import React, {useEffect, useRef} from 'react';
 
 const MODES = {
 	css: {
