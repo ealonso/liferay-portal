b615a4cc109c6c768ad2d61595b20cbb1883b83e
Sandro Chinea <sandrochinea@gmail.com>
2020-03-18 14:39:00 +0000

    LPS-110111 Use ControlsId for handling drag and drop

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/useDragAndDrop.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/useDragAndDrop.js
@@ -18,6 +18,7 @@ import {useDrag, useDrop} from 'react-dnd';
 import {getEmptyImage} from 'react-dnd-html5-backend';
 
 import {LAYOUT_DATA_ITEM_TYPES} from '../config/constants/layoutDataItemTypes';
+import {useFromControlsId, useToControlsId} from './ControlsIdConverterContext';
 
 const LAYOUT_DATA_ALLOWED_CHILDREN_TYPES = {
 	[LAYOUT_DATA_ITEM_TYPES.root]: [
@@ -130,6 +131,9 @@ export default function useDragAndDrop({
 		store: {dropTargetItemId, targetPosition},
 	} = useContext(DragDropManagerImpl);
 
+	const fromControlsId = useFromControlsId();
+	const toControlsId = useToControlsId();
+
 	const [dragOptions, drag, preview] = useDrag({
 		collect: _monitor => ({
 			isDragging: _monitor.isDragging(),
@@ -165,7 +169,7 @@ export default function useDragAndDrop({
 				const {parentId, position} = getParentItemIdAndPositon({
 					item: _item,
 					items: layoutData.items,
-					siblingOrParentId: dropTargetItemId,
+					siblingOrParentId: fromControlsId(dropTargetItemId),
 					targetPosition,
 				});
 
@@ -230,7 +234,7 @@ export default function useDragAndDrop({
 			switch (result) {
 				case RULES_TYPE.MIDDLE:
 					dispatch({
-						dropTargetItemId: item.itemId,
+						dropTargetItemId: toControlsId(item.itemId),
 						targetPosition: TARGET_POSITION.MIDDLE,
 					});
 					break;
@@ -238,13 +242,13 @@ export default function useDragAndDrop({
 					dispatch({
 						dropTargetItemId:
 							getAncestorId(layoutData.items[item.parentId]) ||
-							item.itemId,
+							toControlsId(item.itemId),
 						targetPosition: newTargetPosition,
 					});
 					break;
 				case RULES_TYPE.VALID_MOVE:
 					dispatch({
-						dropTargetItemId: item.itemId,
+						dropTargetItemId: toControlsId(item.itemId),
 						targetPosition: newTargetPosition,
 					});
 					break;
