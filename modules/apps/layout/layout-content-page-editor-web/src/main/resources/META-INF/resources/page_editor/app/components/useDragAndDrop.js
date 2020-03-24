21cb0a34c1f3ad919999b0066689d466556f39c6
Sandro Chinea <sandrochinea@gmail.com>
2020-03-24 16:14:12 +0000

    LPS-110111 Don't get controlsId if parent is collection

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/useDragAndDrop.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/useDragAndDrop.js
@@ -84,7 +84,8 @@ const getAncestorId = (parent, toControlsId) => {
 			? parent.parentId
 			: parent.itemId;
 
-	return parent.type === LAYOUT_DATA_ITEM_TYPES.collectionItem
+	return parent.type === LAYOUT_DATA_ITEM_TYPES.collectionItem ||
+		parent.type === LAYOUT_DATA_ITEM_TYPES.collection
 		? ancestorId
 		: toControlsId(ancestorId);
 };
