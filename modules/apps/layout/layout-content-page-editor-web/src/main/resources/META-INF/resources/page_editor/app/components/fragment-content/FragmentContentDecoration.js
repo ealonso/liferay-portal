ee7ba8de601b09f9992b5521d61a4a3d70c72968
Sandro Chinea <sandrochinea@gmail.com>
2020-03-24 15:46:20 +0000

    LPS-110111 Use toControlsId for editables decoration

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/fragment-content/FragmentContentDecoration.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/fragment-content/FragmentContentDecoration.js
@@ -27,6 +27,7 @@ import {
 	useIsActive,
 	useIsHovered,
 } from '../Controls';
+import {useToControlsId} from '../ControlsIdConverterContext';
 import {useEditableDecoration} from './EditableDecorationContext';
 import {EDITABLE_DECORATION_CLASS_NAMES} from './EditableDecorationMask';
 import getAllEditables from './getAllEditables';
@@ -44,6 +45,7 @@ export default function FragmentContentDecoration({
 	const isHovered = useIsHovered();
 	const hoveredItemType = useHoveredItemType();
 	const isActive = useIsActive();
+	const toControlsId = useToControlsId();
 	const languageId = useSelector(state => state.languageId);
 	const prefixedSegmentsExperienceId = useSelector(
 		selectPrefixedSegmentsExperienceId
@@ -53,6 +55,8 @@ export default function FragmentContentDecoration({
 		fragmentEntryLinkId,
 		getEditableElementId(editableElement)
 	);
+
+	const editableUniqueControlsId = toControlsId(editableUniqueId);
 
 	const editableValue = useSelector(state =>
 		createSelectEditableValue(
@@ -125,16 +129,16 @@ export default function FragmentContentDecoration({
 
 	useLayoutEffect(() => {
 		if (className) {
-			registerElement(editableUniqueId, editableElement);
-			updateClassName(editableUniqueId, className);
+			registerElement(editableUniqueControlsId, editableElement);
+			updateClassName(editableUniqueControlsId, className);
 		}
 		else {
-			unregisterElement(editableUniqueId);
+			unregisterElement(editableUniqueControlsId);
 		}
 	}, [
 		className,
 		editableElement,
-		editableUniqueId,
+		editableUniqueControlsId,
 		registerElement,
 		unregisterElement,
 		updateClassName,
@@ -142,9 +146,9 @@ export default function FragmentContentDecoration({
 
 	useLayoutEffect(
 		() => () => {
-			unregisterElement(editableUniqueId);
+			unregisterElement(editableUniqueControlsId);
 		},
-		[editableUniqueId, unregisterElement]
+		[editableUniqueControlsId, unregisterElement]
 	);
 
 	return null;
