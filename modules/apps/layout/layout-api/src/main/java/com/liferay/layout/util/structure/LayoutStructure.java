db36c222df57b773d570d7fc76c9ee1bffaabe24
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-04-27 18:11:53 +0200

    LPS-112542 Adds new method to return an empty LayoutStructure

--- modules/apps/layout/layout-api/src/main/java/com/liferay/layout/util/structure/LayoutStructure.java
+++ modules/apps/layout/layout-api/src/main/java/com/liferay/layout/util/structure/LayoutStructure.java
@@ -41,9 +41,17 @@ import java.util.UUID;
  */
 public class LayoutStructure {
 
+	public static LayoutStructure emptyLayoutStructure() {
+		LayoutStructure layoutStructure = new LayoutStructure();
+
+		layoutStructure.addRootLayoutStructureItem();
+
+		return layoutStructure;
+	}
+
 	public static LayoutStructure of(String layoutStructure) {
 		if (Validator.isNull(layoutStructure)) {
-			return new LayoutStructure();
+			return emptyLayoutStructure();
 		}
 
 		try {
@@ -111,7 +119,7 @@ public class LayoutStructure {
 			}
 		}
 
-		return new LayoutStructure();
+		return emptyLayoutStructure();
 	}
 
 	public LayoutStructure() {
