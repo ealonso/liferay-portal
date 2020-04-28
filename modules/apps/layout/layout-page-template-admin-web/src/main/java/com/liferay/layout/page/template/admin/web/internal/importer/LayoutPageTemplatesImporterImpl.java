580da323780043b8328a6cab6d720b0f1678bde8
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-04-28 10:51:47 +0200

    LPS-112729 Uses update instead of delete and add

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/importer/LayoutPageTemplatesImporterImpl.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/importer/LayoutPageTemplatesImporterImpl.java
@@ -175,7 +175,11 @@ public class LayoutPageTemplatesImporterImpl
 				layoutStructure,
 				parentLayoutStructureItem.getChildrenItemIds()));
 
-		_updateLayoutPageTemplateStructure(layout, layoutStructure);
+		_layoutPageTemplateStructureLocalService.
+			updateLayoutPageTemplateStructure(
+				layout.getGroupId(),
+				_portal.getClassNameId(Layout.class.getName()),
+				layout.getPlid(), layoutStructure.toString());
 
 		return fragmentEntryLinks;
 	}
@@ -1056,7 +1060,11 @@ public class LayoutPageTemplatesImporterImpl
 			}
 		}
 
-		_updateLayoutPageTemplateStructure(layout, layoutStructure);
+		_layoutPageTemplateStructureLocalService.
+			updateLayoutPageTemplateStructure(
+				layout.getGroupId(),
+				_portal.getClassNameId(Layout.class.getName()),
+				layout.getPlid(), layoutStructure.toString());
 
 		_updateLayouts(layoutPageTemplateEntry);
 	}
@@ -1172,30 +1180,6 @@ public class LayoutPageTemplatesImporterImpl
 		}
 
 		return null;
-	}
-
-	private void _updateLayoutPageTemplateStructure(
-			Layout layout, LayoutStructure layoutStructure)
-		throws Exception {
-
-		long classNameId = _portal.getClassNameId(Layout.class.getName());
-
-		JSONObject jsonObject = layoutStructure.toJSONObject();
-
-		LayoutPageTemplateStructure layoutPageTemplateStructure =
-			_layoutPageTemplateStructureLocalService.
-				fetchLayoutPageTemplateStructure(
-					layout.getGroupId(), classNameId, layout.getPlid());
-
-		if (layoutPageTemplateStructure != null) {
-			_layoutPageTemplateStructureLocalService.
-				deleteLayoutPageTemplateStructure(layoutPageTemplateStructure);
-		}
-
-		_layoutPageTemplateStructureLocalService.addLayoutPageTemplateStructure(
-			layout.getUserId(), layout.getGroupId(), classNameId,
-			layout.getPlid(), jsonObject.toString(),
-			ServiceContextThreadLocal.getServiceContext());
 	}
 
 	private void _updateLayouts(LayoutPageTemplateEntry layoutPageTemplateEntry)
