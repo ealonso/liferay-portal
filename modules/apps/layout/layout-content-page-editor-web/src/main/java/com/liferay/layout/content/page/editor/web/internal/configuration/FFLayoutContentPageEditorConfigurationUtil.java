483fb7e2e0cc5e95f72cc0faa09947ee98b97e9e
Victor Galan <victor.galan@liferay.com>
2020-05-14 11:44:28 +0000

    LPS-113673 Create a Util class for accessing the feature flag configurations

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/configuration/FFLayoutContentPageEditorConfigurationUtil.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.content.page.editor.web.internal.configuration;
+
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+
+/**
+ * @author Víctor Galán
+ */
+@Component(
+	configurationPid = "com.liferay.layout.content.page.editor.web.internal.configuration.FFLayoutContentPageEditorConfiguration",
+	immediate = true, service = FFLayoutContentPageEditorConfigurationUtil.class
+)
+public class FFLayoutContentPageEditorConfigurationUtil {
+
+	public static boolean responsiveEnabled() {
+		return _ffLayoutContentPageEditorConfiguration.responsiveEnabled();
+	}
+
+	public static boolean undoEnabled() {
+		return _ffLayoutContentPageEditorConfiguration.undoEnabled();
+	}
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		_ffLayoutContentPageEditorConfiguration =
+			ConfigurableUtil.createConfigurable(
+				FFLayoutContentPageEditorConfiguration.class, properties);
+	}
+
+	private static volatile FFLayoutContentPageEditorConfiguration
+		_ffLayoutContentPageEditorConfiguration;
+
+}
