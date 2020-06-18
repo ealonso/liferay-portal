0f2ad890c84de64de4a802fc75ee64b87541136c
Pavel Savinov <pavel.savinov@liferay.com>
2020-06-18 15:23:52 +0200

    LPS-112735 Add OOTB web content vocabularies on the instance level

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/instance/lifecycle/AddDefaultAssetVocabulariesPortalInstanceLifecycleListener.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/instance/lifecycle/AddDefaultAssetVocabulariesPortalInstanceLifecycleListener.java
@@ -16,41 +16,30 @@ package com.liferay.journal.internal.instance.lifecycle;
 
 import com.liferay.asset.kernel.model.AssetVocabulary;
 import com.liferay.asset.kernel.service.AssetVocabularyLocalService;
-import com.liferay.dynamic.data.mapping.util.DefaultDDMStructureHelper;
-import com.liferay.journal.configuration.JournalServiceConfiguration;
 import com.liferay.journal.model.JournalArticle;
-import com.liferay.journal.service.JournalArticleLocalService;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener;
 import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Company;
-import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
-import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.service.ServiceContextFactory;
-import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Modified;
-import org.osgi.service.component.annotations.Reference;
 
 import java.util.Collections;
 import java.util.Locale;
-import java.util.Map;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Pavel Savinov
  */
-@Component(
-	immediate = true, service = PortalInstanceLifecycleListener.class
-)
+@Component(immediate = true, service = PortalInstanceLifecycleListener.class)
 public class AddDefaultAssetVocabulariesPortalInstanceLifecycleListener
 	extends BasePortalInstanceLifecycleListener {
 
@@ -75,11 +64,29 @@ public class AddDefaultAssetVocabulariesPortalInstanceLifecycleListener
 			_portal.getClassNameId(JournalArticle.class) + ":-1");
 
 		if (audienceVocabulary == null) {
+			ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+				defaultLocale, getClass());
+
 			_assetVocabularyLocalService.addVocabulary(
 				defaultUser.getUserId(), company.getGroupId(), "audience",
 				StringPool.BLANK,
 				Collections.singletonMap(
-					defaultLocale, LanguageUtil.get(defaultLocale, "audience")),
+					defaultLocale,
+					LanguageUtil.get(resourceBundle, "audience")),
+				Collections.emptyMap(), unicodeProperties.toString(),
+				serviceContext);
+		}
+
+		AssetVocabulary stageVocabulary =
+			_assetVocabularyLocalService.fetchGroupVocabulary(
+				company.getGroupId(), "stage");
+
+		if (stageVocabulary == null) {
+			_assetVocabularyLocalService.addVocabulary(
+				defaultUser.getUserId(), company.getGroupId(), "stage",
+				StringPool.BLANK,
+				Collections.singletonMap(
+					defaultLocale, LanguageUtil.get(defaultLocale, "stage")),
 				Collections.emptyMap(), unicodeProperties.toString(),
 				serviceContext);
 		}
