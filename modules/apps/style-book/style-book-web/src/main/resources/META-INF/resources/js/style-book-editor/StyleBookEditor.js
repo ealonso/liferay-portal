2970f0725c4b4a8abbfb4e42ee413b7209476664
Victor Galan <victor.galan@liferay.com>
2020-07-09 15:46:50 +0200

    LPS-116939 Add main components of the application

--- modules/apps/style-book/style-book-web/src/main/resources/META-INF/resources/js/style-book-editor/StyleBookEditor.js
+++ modules/apps/style-book/style-book-web/src/main/resources/META-INF/resources/js/style-book-editor/StyleBookEditor.js
@@ -19,6 +19,23 @@ import PagePreview from './PagePreview';
 import Sidebar from './Sidebar';
 import {StyleBookContextProvider} from './StyleBookContext';
 
+const TOKEN_CATEGORIES = [
+	{
+		name: 'category1',
+		tokenSets: [
+			{name: 'General', tokens: [{name: 'portlet-topper-bg'}]},
+			{name: 'Colors', tokens: [{name: 'primary'}, {name: 'blue'}]},
+		],
+	},
+	{
+		name: 'category2',
+		tokenSets: [
+			{name: 'Custom', tokens: [{name: 'fontSize'}]},
+			{name: 'Colors', tokens: [{name: 'secondary'}, {name: 'blue'}]},
+		],
+	},
+];
+
 const StyleBookEditor = ({
 	namespace,
 	publishURL,
@@ -47,7 +64,7 @@ const StyleBookEditor = ({
 				publishURL,
 				saveDraftURL,
 				setTokenValues,
-				tokenCategories,
+				tokenCategories: TOKEN_CATEGORIES,
 				tokenValues,
 			}}
 		>
