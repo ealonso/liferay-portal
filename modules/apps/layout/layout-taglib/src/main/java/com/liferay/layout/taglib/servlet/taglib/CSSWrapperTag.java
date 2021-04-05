b21ca82a0a648db42a53e73ff1c451ca7c85b02a
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-04-05 14:48:13 +0100

    LPS-X Css wrapper class

---
+++ modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/servlet/taglib/CSSWrapperTag.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.taglib.servlet.taglib;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.BodyContent;
+import javax.servlet.jsp.tagext.BodyTagSupport;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class CSSWrapperTag extends BodyTagSupport {
+
+	@Override
+	public int doEndTag() throws JspException {
+		try {
+			JspWriter jspWriter = pageContext.getOut();
+
+			jspWriter.write("<div class=\"custom-css\">");
+
+			BodyContent bodyContent = getBodyContent();
+
+			jspWriter.write(bodyContent.getString());
+
+			jspWriter.write("</div>");
+		}
+		catch (Exception exception) {
+			throw new JspException(exception);
+		}
+
+		return EVAL_PAGE;
+	}
+
+	@Override
+	public int doStartTag() {
+		return EVAL_BODY_BUFFERED;
+	}
+
+}
