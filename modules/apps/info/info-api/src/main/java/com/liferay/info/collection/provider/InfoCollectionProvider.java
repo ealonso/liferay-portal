a90e873b949fd7be41713de7fb1e3b5fcacb3aaa
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-06-15 14:23:23 +0100

    LPS-130446 Adds new interface to represent info collections

---
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/collection/provider/InfoCollectionProvider.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.info.collection.provider;
+
+import com.liferay.info.pagination.InfoPage;
+import com.liferay.info.type.Keyed;
+import com.liferay.info.type.Labeled;
+import com.liferay.petra.reflect.GenericUtil;
+
+/**
+ * @author Jorge Ferrer
+ */
+public interface InfoCollectionProvider<T> extends Keyed, Labeled {
+
+	public InfoPage<? extends T> getCollectionInfoPage(
+		CollectionQuery collectionQuery);
+
+	public default Class<? extends T> getCollectionItemClass() {
+		return (Class<? extends T>)GenericUtil.getGenericClass(this, 1);
+	}
+
+	public default boolean isAvailable() {
+		return true;
+	}
+
+}
