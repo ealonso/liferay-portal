43b77b1cfd50d9893ca5a74247e2caf7bf9bd88f
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-06-15 14:29:04 +0100

    LPS-X Extends new interface to avoid having InfoListProviders and InfoItemRelatedListProviders

--- modules/apps/info/info-api/src/main/java/com/liferay/info/list/provider/InfoItemRelatedListProvider.java
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/list/provider/InfoItemRelatedListProvider.java
@@ -14,9 +14,6 @@
 
 package com.liferay.info.list.provider;
 
-import com.liferay.info.pagination.InfoPage;
-import com.liferay.info.pagination.Pagination;
-import com.liferay.info.sort.Sort;
 import com.liferay.info.type.Keyed;
 import com.liferay.info.type.Labeled;
 import com.liferay.petra.reflect.GenericUtil;
@@ -24,15 +21,12 @@ import com.liferay.petra.reflect.GenericUtil;
 /**
  * @author Jorge Ferrer
  */
-public interface InfoItemRelatedListProvider<S, R> extends Keyed, Labeled {
+public interface InfoItemRelatedListProvider<S, R>
+	extends InfoItemListProvider<R>, Keyed, Labeled {
 
 	public default Class<? extends R> getRelatedItemClass() {
 		return (Class<? extends R>)GenericUtil.getGenericClass(this, 1);
 	}
-
-	public InfoPage<? extends R> getRelatedItemsInfoPage(
-		S sourceItem, InfoListProviderContext infoListProviderContext,
-		Pagination pagination, Sort sort);
 
 	public default Class<? extends S> getSourceItemClass() {
 		return (Class<? extends S>)GenericUtil.getGenericClass(this, 0);
