e482b779cec7836e5d87bbcfb7fd37ed67811e5a
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-06-24 08:40:01 +0100

    LPS-130446 Replace InfoListProvider for InfoCollectionProvider

--- modules/apps/info/info-api/src/main/java/com/liferay/info/collection/provider/CollectionQuery.java
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/collection/provider/CollectionQuery.java
@@ -23,6 +23,8 @@ import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 /**
