80d4ac2f4d422e7939b377446279411a8cf329ad
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-07-09 13:17:36 +0200

    LPS-135319 Implements new RelatedInfoItemCollectionProvider instead of old InfoItemRelatedListProvider.

--- modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/info/collection/provider/AssetCategoriesForAssetEntryRelatedInfoItemCollectionProvider.java
+++ modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/info/collection/provider/AssetCategoriesForAssetEntryRelatedInfoItemCollectionProvider.java
@@ -12,15 +12,15 @@
  * details.
  */
 
-package com.liferay.asset.categories.admin.web.internal.info.list.provider;
+package com.liferay.asset.categories.admin.web.internal.info.collection.provider;
 
 import com.liferay.asset.entry.rel.model.AssetEntryAssetCategoryRel;
 import com.liferay.asset.entry.rel.service.AssetEntryAssetCategoryRelLocalService;
 import com.liferay.asset.kernel.model.AssetCategory;
 import com.liferay.asset.kernel.model.AssetEntry;
 import com.liferay.asset.kernel.service.AssetCategoryLocalService;
-import com.liferay.info.list.provider.InfoItemRelatedListProvider;
-import com.liferay.info.list.provider.InfoListProviderContext;
+import com.liferay.info.collection.provider.CollectionQuery;
+import com.liferay.info.collection.provider.RelatedInfoItemCollectionProvider;
 import com.liferay.info.pagination.InfoPage;
 import com.liferay.info.pagination.Pagination;
 import com.liferay.info.sort.Sort;
@@ -30,8 +30,10 @@ import com.liferay.portal.kernel.resource.bundle.ResourceBundleLoaderUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import org.osgi.framework.Bundle;
@@ -42,9 +44,9 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Jürgen Kappler
  */
-@Component(immediate = true, service = InfoItemRelatedListProvider.class)
-public class AssetCategoriesForAssetEntryInfoItemRelatedListProvider
-	implements InfoItemRelatedListProvider<AssetEntry, AssetCategory> {
+@Component(immediate = true, service = RelatedInfoItemCollectionProvider.class)
+public class AssetCategoriesForAssetEntryRelatedInfoItemCollectionProvider
+	implements RelatedInfoItemCollectionProvider<AssetCategory, AssetEntry> {
 
 	@Override
 	public String getLabel(Locale locale) {
@@ -62,9 +64,22 @@ public class AssetCategoriesForAssetEntryInfoItemRelatedListProvider
 	}
 
 	@Override
-	public InfoPage<AssetCategory> getRelatedItemsInfoPage(
-		AssetEntry assetEntry, InfoListProviderContext infoListProviderContext,
-		Pagination pagination, Sort sort) {
+	public InfoPage<AssetCategory> getCollectionInfoPage(
+		CollectionQuery collectionQuery) {
+
+		Optional<Object> relatedItemOptional =
+			collectionQuery.getRelatedItemObjectOptional();
+
+		Object relatedItem = relatedItemOptional.orElse(null);
+
+		if (!(relatedItem instanceof AssetEntry)) {
+			return InfoPage.of(
+				Collections.emptyList(), collectionQuery.getPagination(), 0);
+		}
+
+		AssetEntry assetEntry = (AssetEntry)relatedItem;
+
+		Pagination pagination = collectionQuery.getPagination();
 
 		List<AssetEntryAssetCategoryRel> assetEntryAssetCategoryRels =
 			_assetEntryAssetCategoryRelLocalService.
@@ -100,9 +115,14 @@ public class AssetCategoriesForAssetEntryInfoItemRelatedListProvider
 
 						@Override
 						public boolean isAscending() {
-							if (sort == null) {
+							Optional<Sort> sortOptional =
+								collectionQuery.getSortOptional();
+
+							if (!sortOptional.isPresent()) {
 								return true;
 							}
+
+							Sort sort = sortOptional.get();
 
 							if (sort.isReverse()) {
 								return false;
