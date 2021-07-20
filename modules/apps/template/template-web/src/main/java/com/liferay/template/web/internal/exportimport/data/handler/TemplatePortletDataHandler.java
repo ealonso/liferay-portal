a6b5bdec86ebb0c07963d46f00b8b45ab7b519af
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-07-20 13:12:36 +0200

    LPS-135841 Implement export data

--- modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/exportimport/data/handler/TemplatePortletDataHandler.java
+++ modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/exportimport/data/handler/TemplatePortletDataHandler.java
@@ -14,26 +14,38 @@
 
 package com.liferay.template.web.internal.exportimport.data.handler;
 
+import com.liferay.dynamic.data.mapping.constants.DDMTemplateConstants;
 import com.liferay.dynamic.data.mapping.model.DDMTemplate;
 import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalService;
 import com.liferay.exportimport.kernel.lar.BasePortletDataHandler;
+import com.liferay.exportimport.kernel.lar.ExportImportDateUtil;
+import com.liferay.exportimport.kernel.lar.ManifestSummary;
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
 import com.liferay.exportimport.kernel.lar.PortletDataHandler;
 import com.liferay.exportimport.kernel.lar.PortletDataHandlerBoolean;
 import com.liferay.exportimport.kernel.lar.PortletDataHandlerControl;
 import com.liferay.exportimport.kernel.lar.StagedModelType;
 import com.liferay.exportimport.kernel.staging.Staging;
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.ExportActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.Property;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.model.ClassName;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.template.TemplateHandler;
 import com.liferay.portal.kernel.template.TemplateHandlerRegistry;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.xml.Element;
 import com.liferay.template.constants.TemplatePortletKeys;
 import com.liferay.template.web.internal.constants.TemplateConstants;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.portlet.PortletPreferences;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -65,6 +77,29 @@ public class TemplatePortletDataHandler extends BasePortletDataHandler {
 	}
 
 	@Override
+	public long getExportModelCount(ManifestSummary manifestSummary) {
+		long totalModelCount = -1;
+
+		for (StagedModelType stagedModelType : _getStagedModelTypes()) {
+			long modelCount = manifestSummary.getModelAdditionCount(
+				stagedModelType);
+
+			if (modelCount == -1) {
+				continue;
+			}
+
+			if (totalModelCount == -1) {
+				totalModelCount = modelCount;
+			}
+			else {
+				totalModelCount += modelCount;
+			}
+		}
+
+		return totalModelCount;
+	}
+
+	@Override
 	public String getNamespace() {
 		return NAMESPACE;
 	}
@@ -90,9 +125,100 @@ public class TemplatePortletDataHandler extends BasePortletDataHandler {
 		setStagingControls(getExportControls());
 	}
 
+	@Override
+	protected String doExportData(
+			PortletDataContext portletDataContext, String portletId,
+			PortletPreferences portletPreferences)
+		throws Exception {
+
+		Element rootElement = addExportDataRootElement(portletDataContext);
+
+		ActionableDynamicQuery actionableDynamicQuery =
+			_getDDMTemplateActionableDynamicQuery(
+				portletDataContext,
+				ArrayUtil.toArray(_templateHandlerRegistry.getClassNameIds()),
+				new StagedModelType(
+					_portal.getClassNameId(DDMTemplate.class),
+					StagedModelType.REFERRER_CLASS_NAME_ID_ALL));
+
+		actionableDynamicQuery.performActions();
+
+		return getExportDataRootElementString(rootElement);
+	}
+
+	@Override
+	protected void doPrepareManifestSummary(
+			PortletDataContext portletDataContext,
+			PortletPreferences portletPreferences)
+		throws Exception {
+
+		if (ExportImportDateUtil.isRangeFromLastPublishDate(
+				portletDataContext)) {
+
+			_staging.populateLastPublishDateCounts(
+				portletDataContext, _getStagedModelTypes());
+
+			return;
+		}
+
+		for (StagedModelType stagedModelType : _getStagedModelTypes()) {
+			ActionableDynamicQuery actionableDynamicQuery =
+					_getDDMTemplateActionableDynamicQuery(
+					portletDataContext,
+					new Long[] {stagedModelType.getReferrerClassNameId()},
+					stagedModelType);
+
+			actionableDynamicQuery.performCount();
+		}
+	}
+
+	@Reference(unbind = "-")
+	protected void setDDMTemplateLocalService(
+			DDMTemplateLocalService ddmTemplateLocalService) {
+
+		_ddmTemplateLocalService = ddmTemplateLocalService;
+	}
+
 	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
 	protected void setModuleServiceLifecycle(
-		ModuleServiceLifecycle moduleServiceLifecycle) {
+			ModuleServiceLifecycle moduleServiceLifecycle) {
+	}
+
+	private ActionableDynamicQuery _getDDMTemplateActionableDynamicQuery(
+		final PortletDataContext portletDataContext, final Long[] classNameIds,
+		final StagedModelType stagedModelType) {
+
+		ExportActionableDynamicQuery exportActionableDynamicQuery =
+			_ddmTemplateLocalService.getExportActionableDynamicQuery(
+				portletDataContext);
+
+		final ActionableDynamicQuery.AddCriteriaMethod addCriteriaMethod =
+			exportActionableDynamicQuery.getAddCriteriaMethod();
+
+		exportActionableDynamicQuery.setAddCriteriaMethod(
+			dynamicQuery -> {
+				addCriteriaMethod.addCriteria(dynamicQuery);
+
+				Property classNameIdProperty = PropertyFactoryUtil.forName(
+					"classNameId");
+
+				dynamicQuery.add(classNameIdProperty.in(classNameIds));
+
+				Property classPKProperty = PropertyFactoryUtil.forName(
+					"classPK");
+
+				dynamicQuery.add(classPKProperty.eq(0L));
+
+				Property typeProperty = PropertyFactoryUtil.forName("type");
+
+				dynamicQuery.add(
+					typeProperty.eq(
+						DDMTemplateConstants.TEMPLATE_TYPE_DISPLAY));
+			});
+
+		exportActionableDynamicQuery.setStagedModelType(stagedModelType);
+
+		return exportActionableDynamicQuery;
 	}
 
 	private PortletDataHandlerControl[] _getPortletDataHandlerControls() {
@@ -146,10 +272,15 @@ public class TemplatePortletDataHandler extends BasePortletDataHandler {
 	@Reference
 	private ClassNameLocalService _classNameLocalService;
 
+	private DDMTemplateLocalService _ddmTemplateLocalService;
+
 	@Reference
 	private Portal _portal;
 
 	private StagedModelType[] _stagedModelTypes;
+
+	@Reference
+	private Staging _staging;
 
 	@Reference
 	private TemplateHandlerRegistry _templateHandlerRegistry;
