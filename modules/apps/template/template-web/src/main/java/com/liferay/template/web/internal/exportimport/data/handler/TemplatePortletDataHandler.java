a03da9d0fbf1fce9ed56e6face89e9ab1fec3c26
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-07-22 11:06:44 +0200

    LPS-135841 Set specific namespace

--- modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/exportimport/data/handler/TemplatePortletDataHandler.java
+++ modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/exportimport/data/handler/TemplatePortletDataHandler.java
@@ -100,6 +100,11 @@ public class TemplatePortletDataHandler extends BasePortletDataHandler {
 	}
 
 	@Override
+	public String getNamespace() {
+		return NAMESPACE;
+	}
+
+	@Override
 	public String getSchemaVersion() {
 		return SCHEMA_VERSION;
 	}
