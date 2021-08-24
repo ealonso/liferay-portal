39b106da841a5351c6f0544e402894cc10b3c936
Pablo Molina <contact@pablomolina.me>
2021-08-24 15:04:41 +0200

    LPS-137812 Resolve filter label in applied_filters.jsp

--- modules/apps/fragment/fragment-renderer-collection-filter-impl/src/main/java/com/liferay/fragment/renderer/collection/filter/internal/display/context/CollectionAppliedFiltersFragmentRendererDisplayContext.java
+++ modules/apps/fragment/fragment-renderer-collection-filter-impl/src/main/java/com/liferay/fragment/renderer/collection/filter/internal/display/context/CollectionAppliedFiltersFragmentRendererDisplayContext.java
@@ -14,6 +14,8 @@
 
 package com.liferay.fragment.renderer.collection.filter.internal.display.context;
 
+import com.liferay.fragment.collection.filter.FragmentCollectionFilter;
+import com.liferay.fragment.collection.filter.FragmentCollectionFilterTracker;
 import com.liferay.fragment.collection.filter.constants.FragmentCollectionFilterConstants;
 import com.liferay.fragment.constants.FragmentConfigurationFieldDataType;
 import com.liferay.fragment.model.FragmentEntryLink;
@@ -33,6 +35,7 @@ import com.liferay.portal.kernel.util.HashMapBuilder;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -43,16 +46,19 @@ import javax.servlet.http.HttpServletRequest;
 public class CollectionAppliedFiltersFragmentRendererDisplayContext {
 
 	public CollectionAppliedFiltersFragmentRendererDisplayContext(
+		FragmentCollectionFilterTracker fragmentCollectionFilterTracker,
 		FragmentEntryConfigurationParser fragmentEntryConfigurationParser,
 		FragmentEntryLinkLocalService fragmentEntryLinkLocalService,
 		FragmentRendererContext fragmentRendererContext,
 		HttpServletRequest httpServletRequest) {
 
+		_fragmentCollectionFilterTracker = fragmentCollectionFilterTracker;
 		_fragmentEntryConfigurationParser = fragmentEntryConfigurationParser;
 		_fragmentEntryLinkLocalService = fragmentEntryLinkLocalService;
 		_httpServletRequest = httpServletRequest;
 
 		_fragmentEntryLink = fragmentRendererContext.getFragmentEntryLink();
+		_locale = fragmentRendererContext.getLocale();
 	}
 
 	public List<Map<String, String>> getAppliedFilters() {
@@ -113,10 +119,22 @@ public class CollectionAppliedFiltersFragmentRendererDisplayContext {
 				continue;
 			}
 
+			FragmentCollectionFilter fragmentCollectionFilter =
+				_fragmentCollectionFilterTracker.getFragmentCollectionFilter(
+					parameterData.get(1));
+
+			if (fragmentCollectionFilter == null) {
+				continue;
+			}
+
 			for (String filterValue : entry.getValue()) {
 				appliedFilters.add(
 					HashMapBuilder.put(
 						"filterFragmentEntryLinkId", filterFragmentEntryLinkId
+					).put(
+						"filterLabel",
+						fragmentCollectionFilter.getFilterValueLabel(
+							filterValue, _locale)
 					).put(
 						"filterType", parameterData.get(1)
 					).put(
@@ -156,10 +174,13 @@ public class CollectionAppliedFiltersFragmentRendererDisplayContext {
 	}
 
 	private Map<String, Object> _collectionAppliedFiltersProps;
+	private final FragmentCollectionFilterTracker
+		_fragmentCollectionFilterTracker;
 	private final FragmentEntryConfigurationParser
 		_fragmentEntryConfigurationParser;
 	private final FragmentEntryLink _fragmentEntryLink;
 	private final FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
 	private final HttpServletRequest _httpServletRequest;
+	private final Locale _locale;
 
 }
