43d19f6fab8126f12679b1f71a4e660e15e560dd
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-02-02 17:02:51 +0100

    LPS-139522 Copy from JournalArticleIndexer, no logic changes

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalArticleIndexer.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalArticleIndexer.java
@@ -111,19 +111,6 @@ public class JournalArticleIndexer extends BaseIndexer<JournalArticle> {
 
 	public static final String CLASS_NAME = JournalArticle.class.getName();
 
-	public JournalArticleIndexer() {
-		setDefaultSelectedFieldNames(
-			Field.ASSET_TAG_NAMES, Field.ARTICLE_ID, Field.COMPANY_ID,
-			Field.DEFAULT_LANGUAGE_ID, Field.ENTRY_CLASS_NAME,
-			Field.ENTRY_CLASS_PK, Field.GROUP_ID, Field.MODIFIED_DATE,
-			Field.SCOPE_GROUP_ID, Field.VERSION, Field.UID);
-		setDefaultSelectedLocalizedFieldNames(
-			Field.CONTENT, Field.DESCRIPTION, Field.TITLE);
-		setFilterSearch(true);
-		setPermissionAware(true);
-		setSelectAllLocales(true);
-	}
-
 	@Override
 	public String getClassName() {
 		return CLASS_NAME;
