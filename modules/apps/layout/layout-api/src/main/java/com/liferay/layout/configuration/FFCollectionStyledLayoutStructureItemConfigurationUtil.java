364562308806c4346439b8cce37bba7e995a249c
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-03-01 08:49:11 +0100

    LPS-147252 Adds util to access to feature flag configuration

---
+++ modules/apps/layout/layout-api/src/main/java/com/liferay/layout/configuration/FFCollectionStyledLayoutStructureItemConfigurationUtil.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.configuration;
+
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+public class FFCollectionStyledLayoutStructureItemConfigurationUtil {
+
+	public static boolean paginationImprovementsEnabled() {
+		FFCollectionStyledLayoutStructureItemConfiguration
+			ffCollectionStyledLayoutStructureItemConfiguration =
+				ConfigurableUtil.createConfigurable(
+					FFCollectionStyledLayoutStructureItemConfiguration.class,
+					HashMapBuilder.put(
+						"configurationPid",
+						"com.liferay.layout.configuration.FFCollectionStyledLayoutStructureItemConfiguration"
+					).build());
+
+		return ffCollectionStyledLayoutStructureItemConfiguration.
+			paginationImprovementsEnabled();
+	}
+
+}
