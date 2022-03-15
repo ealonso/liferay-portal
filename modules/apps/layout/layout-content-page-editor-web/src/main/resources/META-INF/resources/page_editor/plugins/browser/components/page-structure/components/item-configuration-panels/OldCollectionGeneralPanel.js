1db521da2eca97701d7df299ecbc6961b5319102
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2022-03-15 16:33:41 +0100

    LPS-149529 Add new label for list style

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/browser/components/page-structure/components/item-configuration-panels/OldCollectionGeneralPanel.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/browser/components/page-structure/components/item-configuration-panels/OldCollectionGeneralPanel.js
@@ -383,13 +383,19 @@ export function OldCollectionGeneralPanel({item}) {
 						<>
 							<ClayForm.Group small>
 								<label htmlFor={listStyleId}>
-									{Liferay.Language.get('list-style')}
+									{config.featureFlagLps119551
+										? Liferay.Language.get('style-display')
+										: Liferay.Language.get('list-style')}
 								</label>
 
 								<ClaySelectWithOption
-									aria-label={Liferay.Language.get(
-										'list-style'
-									)}
+									aria-label={
+										config.featureFlagLps119551
+											? Liferay.Language.get(
+													'style-display'
+											  )
+											: Liferay.Language.get('list-style')
+									}
 									id={listStyleId}
 									onChange={(event) =>
 										handleConfigurationChanged({
