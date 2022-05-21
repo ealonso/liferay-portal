e51748b9f286bb2b417b07e915387345c46648e3
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-05-21 13:10:21 +0200

    LPS-XXX Implements FragmentEntryConfigurationProviderTracker

---
+++ modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/util/configuration/provider/FragmentEntryConfigurationProviderTrackerImpl.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.internal.util.configuration.provider;
+
+import com.liferay.fragment.util.configuration.provider.FragmentEntryConfigurationProvider;
+import com.liferay.fragment.util.configuration.provider.FragmentEntryConfigurationProviderTracker;
+import com.liferay.osgi.service.tracker.collections.map.PropertyServiceReferenceComparator;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(service = FragmentEntryConfigurationProviderTracker.class)
+public class FragmentEntryConfigurationProviderTrackerImpl
+	implements FragmentEntryConfigurationProviderTracker {
+
+	@Override
+	public FragmentEntryConfigurationProvider
+		getFragmentEntryConfigurationProvider(String type) {
+
+		return _fragmentEntryConfigurationProviderServiceTrackerMap.getService(
+			type);
+	}
+
+	@Override
+	public List<FragmentEntryConfigurationProvider>
+		getFragmentEntryConfigurationProviders() {
+
+		return new ArrayList(
+			_fragmentEntryConfigurationProviderServiceTrackerMap.values());
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_fragmentEntryConfigurationProviderServiceTrackerMap =
+			ServiceTrackerMapFactory.openSingleValueMap(
+				bundleContext,
+				(Class<FragmentEntryConfigurationProvider>)
+					(Class<?>)FragmentEntryConfigurationProvider.class,
+				null,
+				(serviceReference, emitter) -> {
+					FragmentEntryConfigurationProvider
+						fragmentEntryConfigurationProvider =
+							bundleContext.getService(serviceReference);
+
+					try {
+						emitter.emit(
+							fragmentEntryConfigurationProvider.getType());
+					}
+					finally {
+						bundleContext.ungetService(serviceReference);
+					}
+				},
+				new PropertyServiceReferenceComparator<>("service.ranking"));
+	}
+
+	private ServiceTrackerMap<String, FragmentEntryConfigurationProvider>
+		_fragmentEntryConfigurationProviderServiceTrackerMap;
+
+}
