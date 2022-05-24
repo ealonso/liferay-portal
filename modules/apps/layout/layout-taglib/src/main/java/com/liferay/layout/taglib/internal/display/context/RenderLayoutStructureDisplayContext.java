9d4aa4e88271e087fdc404904150fdd20e131630
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-05-24 17:34:57 +0100

    LPS-XXX Sets info form when we are rendering a content page

--- modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/internal/display/context/RenderLayoutStructureDisplayContext.java
+++ modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/internal/display/context/RenderLayoutStructureDisplayContext.java
@@ -24,6 +24,7 @@ import com.liferay.frontend.token.definition.FrontendTokenDefinitionRegistry;
 import com.liferay.frontend.token.definition.FrontendTokenMapping;
 import com.liferay.info.constants.InfoDisplayWebKeys;
 import com.liferay.info.field.InfoFieldValue;
+import com.liferay.info.form.InfoForm;
 import com.liferay.info.item.ClassPKInfoItemIdentifier;
 import com.liferay.info.item.InfoItemDetails;
 import com.liferay.info.item.InfoItemIdentifier;
@@ -476,8 +477,8 @@ public class RenderLayoutStructureDisplayContext {
 	}
 
 	public DefaultFragmentRendererContext getDefaultFragmentRendererContext(
-		FragmentEntryLink fragmentEntryLink, String itemId,
-		int collectionElementIndex) {
+		int collectionElementIndex, FragmentEntryLink fragmentEntryLink,
+		InfoForm infoForm, String itemId) {
 
 		DefaultFragmentRendererContext defaultFragmentRendererContext =
 			new DefaultFragmentRendererContext(fragmentEntryLink);
@@ -498,6 +499,7 @@ public class RenderLayoutStructureDisplayContext {
 			defaultFragmentRendererContext.setPreviewType(_getPreviewType());
 			defaultFragmentRendererContext.setPreviewVersion(
 				_getPreviewVersion());
+			defaultFragmentRendererContext.setInfoForm(infoForm);
 			defaultFragmentRendererContext.setSegmentsEntryIds(
 				_getSegmentsEntryIds());
 		}
