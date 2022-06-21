e64c473f9888304b02562c6353b50b82a63bb7d4
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-06-21 16:14:20 +0100

    LPS-156518 Adds new attribute to select info field to allow have an autocomplete url, for cases with many options

--- modules/apps/info/info-api/src/main/java/com/liferay/info/field/type/SelectInfoFieldType.java
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/field/type/SelectInfoFieldType.java
@@ -24,6 +24,9 @@ import java.util.Locale;
  */
 public class SelectInfoFieldType implements InfoFieldType {
 
+	public static final Attribute<SelectInfoFieldType, String>
+		AUTOCOMPLETE_URL = new Attribute<>();
+
 	public static final SelectInfoFieldType INSTANCE =
 		new SelectInfoFieldType();
 
