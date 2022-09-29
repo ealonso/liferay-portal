0f01248dca75b3943ce6dc69b9af68e0ddf480cb
Diego Hu <diego.hu@liferay.com>
2022-09-29 17:22:09 +0200

    LPS-161798 add replace function to update editable view

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/EditableFragmentEntryProcessor.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/EditableFragmentEntryProcessor.java
@@ -35,10 +35,14 @@ import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
+
+import java.text.ParseException;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -304,7 +308,17 @@ public class EditableFragmentEntryProcessor implements FragmentEntryProcessor {
 				configJSONObject = localizedJSONObject;
 			}
 
-			editableElementParser.replace(element, value, configJSONObject);
+			try {
+				editableElementParser.replace(element, value, configJSONObject);
+			}
+			catch (ParseException parseException) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(parseException);
+				}
+
+				throw new IllegalArgumentException(
+					"Unable to parse date from " + value, parseException);
+			}
 
 			if (!Objects.equals(
 					fragmentEntryProcessorContext.getMode(),
@@ -612,6 +626,9 @@ public class EditableFragmentEntryProcessor implements FragmentEntryProcessor {
 	}
 
 	private static final String[] _REQUIRED_ATTRIBUTE_NAMES = {"id", "type"};
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		EditableFragmentEntryProcessor.class);
 
 	private final Map<String, EditableElementMapper> _editableElementMappers =
 		new HashMap<>();
