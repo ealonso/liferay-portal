5c42719c3ab9bc00ff74d3e7543623141d3d215b
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-09-30 09:06:14 +0200

    LPS-164507 Update layout content every time we publish the page

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/PublishLayoutMVCActionCommand.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/PublishLayoutMVCActionCommand.java
@@ -17,8 +17,11 @@ package com.liferay.layout.content.page.editor.web.internal.portlet.action;
 import com.liferay.exportimport.kernel.staging.LayoutStagingUtil;
 import com.liferay.layout.content.page.editor.constants.ContentPageEditorPortletKeys;
 import com.liferay.layout.content.page.editor.web.internal.util.layout.structure.LayoutStructureUtil;
+import com.liferay.layout.service.LayoutLocalizationLocalService;
+import com.liferay.layout.util.LayoutContentHelper;
 import com.liferay.layout.util.LayoutCopyHelper;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutRevision;
 import com.liferay.portal.kernel.model.LayoutSet;
@@ -34,6 +37,7 @@ import com.liferay.portal.kernel.servlet.MultiSessionMessages;
 import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.Validator;
@@ -43,6 +47,7 @@ import com.liferay.portal.kernel.workflow.WorkflowHandlerRegistryUtil;
 import com.liferay.sites.kernel.util.Sites;
 
 import java.util.Collections;
+import java.util.Locale;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -195,7 +200,18 @@ public class PublishLayoutMVCActionCommand
 
 			_layoutLocalService.updateLayout(layout);
 
+			_updateLayoutContent(layout);
 			_updateLayoutRevision(layout, serviceContext);
+		}
+	}
+
+	private void _updateLayoutContent(Layout layout) {
+		for (Locale locale :
+				_language.getAvailableLocales(layout.getGroupId())) {
+
+			_layoutLocalizationLocalService.updateLayoutLocalization(
+				_layoutContentHelper.getLayoutContent(layout, locale),
+				LocaleUtil.toLanguageId(locale), layout.getPlid());
 		}
 	}
 
@@ -222,7 +238,16 @@ public class PublishLayoutMVCActionCommand
 	}
 
 	@Reference
+	private Language _language;
+
+	@Reference
+	private LayoutContentHelper _layoutContentHelper;
+
+	@Reference
 	private LayoutCopyHelper _layoutCopyHelper;
+
+	@Reference
+	private LayoutLocalizationLocalService _layoutLocalizationLocalService;
 
 	@Reference
 	private LayoutLocalService _layoutLocalService;
