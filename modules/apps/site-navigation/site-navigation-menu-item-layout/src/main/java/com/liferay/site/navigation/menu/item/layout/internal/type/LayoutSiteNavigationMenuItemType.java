7666b7e3fca8adc3bd6d76f1567601e35e6dfc56
Rubén Pulido <ruben.pulido@liferay.com>
2022-10-05 00:18:40 +0200

    LPS-163606 When performing an export from the Navigation Menu section on the Product Menu, ensure that navigation menu items referencing public and private pages can be exported simultaneously

--- modules/apps/site-navigation/site-navigation-menu-item-layout/src/main/java/com/liferay/site/navigation/menu/item/layout/internal/type/LayoutSiteNavigationMenuItemType.java
+++ modules/apps/site-navigation/site-navigation-menu-item-layout/src/main/java/com/liferay/site/navigation/menu/item/layout/internal/type/LayoutSiteNavigationMenuItemType.java
@@ -110,7 +110,9 @@ public class LayoutSiteNavigationMenuItemType
 
 		boolean privateLayout = layout.isPrivateLayout();
 
-		if (privateLayout != portletDataContext.isPrivateLayout()) {
+		if (!ExportImportThreadLocal.isPortletExportInProcess() &&
+			(privateLayout != portletDataContext.isPrivateLayout())) {
+
 			if (_log.isWarnEnabled()) {
 				_log.warn(
 					StringBundler.concat(
@@ -129,7 +131,8 @@ public class LayoutSiteNavigationMenuItemType
 		if (!ArrayUtil.contains(
 				portletDataContext.getLayoutIds(), layout.getLayoutId())) {
 
-			return false;
+				return false;
+			}
 		}
 
 		LayoutRevision layoutRevision = _layoutStaging.getLayoutRevision(
