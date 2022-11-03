e1a99baf7d03319b2f720c9baa36acb9c1bbec02
BarbaraCabrera <barbara.cabrera@liferay.com>
2022-11-03 11:32:08 +0000

    LPS-166857 Moves to layout-impl

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/importer/LayoutStructureItemImporterTracker.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/importer/LayoutStructureItemImporterTracker.java
@@ -12,13 +12,14 @@
  * details.
  */
 
-package com.liferay.layout.page.template.admin.web.internal.headless.delivery.dto.v1_0.structure.importer;
+package com.liferay.layout.internal.importer;
 
 import com.liferay.headless.delivery.dto.v1_0.PageElement;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import com.liferay.layout.internal.importer.structure.util.LayoutStructureItemImporter;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
