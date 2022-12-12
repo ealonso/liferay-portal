c0c65d165a2f521a5c7a03d0e045cc9d8c176853
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-12-12 19:04:43 +0100

    LPS-170962 Uses new method and keep the old one as fallback

--- modules/apps/asset/asset-taglib/src/main/java/com/liferay/asset/taglib/internal/display/context/InputAssetLinksDisplayContext.java
+++ modules/apps/asset/asset-taglib/src/main/java/com/liferay/asset/taglib/internal/display/context/InputAssetLinksDisplayContext.java
@@ -64,6 +64,7 @@ import java.util.Locale;
 import java.util.Map;
 
 import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
 import javax.portlet.PortletURL;
 
 import javax.servlet.http.HttpServletRequest;
@@ -358,31 +359,47 @@ public class InputAssetLinksDisplayContext {
 	private PortletURL _getAssetEntryItemSelectorPortletURL(
 		AssetRendererFactory<?> assetRendererFactory, long subtypeSelectionId) {
 
-		AssetEntryItemSelectorCriterion assetEntryItemSelectorCriterion =
-			new AssetEntryItemSelectorCriterion();
+		PortletRequest portletRequest =
+			(PortletRequest)_httpServletRequest.getAttribute(
+				JavaConstants.JAVAX_PORTLET_REQUEST);
 
-		assetEntryItemSelectorCriterion.setDesiredItemSelectorReturnTypes(
-			new AssetEntryItemSelectorReturnType());
-		assetEntryItemSelectorCriterion.setGroupId(
-			_themeDisplay.getScopeGroupId());
-		assetEntryItemSelectorCriterion.setSelectedGroupIds(
-			new long[] {_themeDisplay.getScopeGroupId()});
-		assetEntryItemSelectorCriterion.setShowNonindexable(true);
-		assetEntryItemSelectorCriterion.setShowScheduled(true);
-		assetEntryItemSelectorCriterion.setSubtypeSelectionId(
-			subtypeSelectionId);
-		assetEntryItemSelectorCriterion.setTypeSelection(
-			assetRendererFactory.getClassName());
+		PortletResponse portletResponse =
+			(PortletResponse)_httpServletRequest.getAttribute(
+				JavaConstants.JAVAX_PORTLET_RESPONSE);
 
-		ItemSelector itemSelector = ItemSelectorUtil.getItemSelector();
+		PortletURL portletURL = assetRendererFactory.getItemSelectorURL(
+			PortalUtil.getLiferayPortletRequest(portletRequest),
+			PortalUtil.getLiferayPortletResponse(portletResponse),
+			subtypeSelectionId, portletResponse.getNamespace() + "selectAsset",
+			true, _assetEntryId);
 
-		PortletURL portletURL = itemSelector.getItemSelectorURL(
-			RequestBackedPortletURLFactoryUtil.create(_portletRequest),
-			getEventName(), assetEntryItemSelectorCriterion);
+		if (portletURL == null) {
+			AssetEntryItemSelectorCriterion assetEntryItemSelectorCriterion =
+				new AssetEntryItemSelectorCriterion();
 
-		if (_assetEntryId > 0) {
-			portletURL.setParameter(
-				"refererAssetEntryId", String.valueOf(_assetEntryId));
+			assetEntryItemSelectorCriterion.setDesiredItemSelectorReturnTypes(
+				new AssetEntryItemSelectorReturnType());
+			assetEntryItemSelectorCriterion.setGroupId(
+				_themeDisplay.getScopeGroupId());
+			assetEntryItemSelectorCriterion.setSelectedGroupIds(
+				new long[] {_themeDisplay.getScopeGroupId()});
+			assetEntryItemSelectorCriterion.setShowNonindexable(true);
+			assetEntryItemSelectorCriterion.setShowScheduled(true);
+			assetEntryItemSelectorCriterion.setSubtypeSelectionId(
+				subtypeSelectionId);
+			assetEntryItemSelectorCriterion.setTypeSelection(
+				assetRendererFactory.getClassName());
+
+			ItemSelector itemSelector = ItemSelectorUtil.getItemSelector();
+
+			portletURL = itemSelector.getItemSelectorURL(
+				RequestBackedPortletURLFactoryUtil.create(_portletRequest),
+				getEventName(), assetEntryItemSelectorCriterion);
+
+			if (_assetEntryId > 0) {
+				portletURL.setParameter(
+					"refererAssetEntryId", String.valueOf(_assetEntryId));
+			}
 		}
 
 		return portletURL;
