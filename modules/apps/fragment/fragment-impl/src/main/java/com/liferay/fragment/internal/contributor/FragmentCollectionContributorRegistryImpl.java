842e9836d2b2f34539986336c28d221723bd9a0a
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-03-15 13:40:03 +0100

    LPS-178527 Uses new method

--- modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/contributor/FragmentCollectionContributorRegistryImpl.java
+++ modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/contributor/FragmentCollectionContributorRegistryImpl.java
@@ -39,6 +39,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
@@ -217,18 +218,24 @@ public class FragmentCollectionContributorRegistryImpl
 	@Reference
 	protected FragmentEntryValidator fragmentEntryValidator;
 
-	private void _updateFragmentEntryLinks(FragmentEntry fragmentEntry) {
+	private void _updateFragmentEntryLinks(
+		Map<String, FragmentEntry> fragmentEntries) {
+
 		_companyLocalService.forEachCompany(
 			company -> {
+				Set<String> fragmentEntriesSet = fragmentEntries.keySet();
+
 				List<FragmentEntryLink> fragmentEntryLinks =
 					_fragmentEntryLinkLocalService.getFragmentEntryLinks(
 						company.getCompanyId(),
-						fragmentEntry.getFragmentEntryKey());
+						fragmentEntriesSet.toArray(new String[0]));
 
 				for (FragmentEntryLink fragmentEntryLink : fragmentEntryLinks) {
 					try {
 						_fragmentEntryLinkLocalService.updateLatestChanges(
-							fragmentEntry, fragmentEntryLink);
+							fragmentEntries.get(
+								fragmentEntryLink.getRendererKey()),
+							fragmentEntryLink);
 					}
 					catch (PortalException portalException) {
 						_log.error(portalException);
@@ -324,9 +331,9 @@ public class FragmentCollectionContributorRegistryImpl
 
 				fragmentEntries.put(
 					fragmentEntry.getFragmentEntryKey(), fragmentEntry);
-
-				_updateFragmentEntryLinks(fragmentEntry);
 			}
+
+			_updateFragmentEntryLinks(fragmentEntries);
 
 			return new FragmentCollectionBag(
 				fragmentCollectionContributor, fragmentCompositions,
