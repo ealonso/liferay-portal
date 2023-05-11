27bed88af63eb8bcb55ba19aa26cf0f11fee0125
clara izquierdo <claraizquierdosanz@gmail.com>
2023-05-11 11:06:57 +0200

    LPS-173150 Add ImportModal component for importing page templates

---
+++ modules/apps/layout/layout-page-template-admin-web/src/main/resources/META-INF/resources/js/addIconAction.js
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {render} from '@liferay/frontend-js-react-web';
+
+import ImportModal from './modal/ImportModal';
+
+export default function ({namespace, url}) {
+	Liferay.Util.setPortletConfigurationIconAction(`${namespace}import`, () => {
+		if (Liferay.FeatureFlags['LPS-174939']) {
+			render(
+				ImportModal,
+				{
+					namespace,
+				},
+				document.createElement('div')
+			);
+		}
+		else {
+			Liferay.Util.openModal({
+				buttons: [
+					{
+						displayType: 'secondary',
+						label: Liferay.Language.get('cancel'),
+						type: 'cancel',
+					},
+					{
+						label: Liferay.Language.get('import'),
+						type: 'submit',
+					},
+				],
+				onClose() {
+					window.location.reload();
+				},
+				title: Liferay.Language.get('import'),
+				url,
+			});
+		}
+	});
+}
