5f9cf5caa197237882ddf8a7969476e26e05ece8
clara izquierdo <claraizquierdosanz@gmail.com>
2023-05-11 21:25:58 +0200

    LPS-173150 Fix overwrite checkboxes in import modals

--- modules/apps/layout/layout-page-template-admin-web/src/main/resources/META-INF/resources/js/modal/ImportModal.tsx
+++ modules/apps/layout/layout-page-template-admin-web/src/main/resources/META-INF/resources/js/modal/ImportModal.tsx
@@ -29,6 +29,7 @@ const ImportModal = ({portletNamespace}: Props) => {
 	const [visible, setVisible] = useState(true);
 	const [error, setError] = useState('');
 	const [isValidForm, setIsValidForm] = useState(false);
+	const [overwrite, setOverwrite] = useState(true);
 
 	const {observer, onClose} = useModal({
 		onClose: () => setVisible(false),
@@ -128,13 +129,14 @@ const ImportModal = ({portletNamespace}: Props) => {
 						</ClayForm.Group>
 
 						<ClayCheckbox
-							checked={true}
+							checked={overwrite}
 							data-testid={`${portletNamespace}overwrite`}
 							id={`${portletNamespace}overwrite`}
 							label={Liferay.Language.get(
 								'overwrite-existing-entries'
 							)}
 							name={`${portletNamespace}overwrite`}
+							onChange={() => setOverwrite(val => !val)}
 						/>
 					</ClayModal.Body>
 
