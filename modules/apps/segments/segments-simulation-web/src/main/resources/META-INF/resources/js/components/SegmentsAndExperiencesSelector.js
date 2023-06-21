5a2a168ce6dce90603d0e494763bf55f9c4d7d90
clara izquierdo <claraizquierdosanz@gmail.com>
2023-06-21 22:53:43 +0200

    LPS-187909 Open item selector url

--- modules/apps/segments/segments-simulation-web/src/main/resources/META-INF/resources/js/components/SegmentsAndExperiencesSelector.js
+++ modules/apps/segments/segments-simulation-web/src/main/resources/META-INF/resources/js/components/SegmentsAndExperiencesSelector.js
@@ -20,7 +20,7 @@ import {ClaySelectWithOption} from '@clayui/form';
 import Label from '@clayui/label';
 import Layout from '@clayui/layout';
 import ClayLink from '@clayui/link';
-import {fetch} from 'frontend-js-web';
+import {fetch, openSelectionModal, sub} from 'frontend-js-web';
 import React, {useCallback, useEffect, useRef, useState} from 'react';
 
 const PREVIEW_OPTIONS = [
@@ -39,10 +39,13 @@ const MAXIMUM_DROPDOWN_ENTRIES = 8;
 function SegmentsAndExperiencesSelector({
 	deactivateSimulationURL,
 	namespace,
+	portletNamespace,
 	segmentationEnabled,
 	segmentsCompanyConfigurationURL,
 	segmentsEntries,
 	segmentsExperiences,
+	selectSegmentsEntryURL,
+	selectSegmentsExperienceURL,
 	showEmptyMessage,
 	simulateSegmentsEntriesURL,
 }) {
@@ -107,11 +110,35 @@ function SegmentsAndExperiencesSelector({
 		}
 	}, []);
 
-	const handleMoreButtonClick = useCallback(() => {
+	const handleMoreSegmentEntriesButtonClick = () => {
+		openSelectionModal({
+			onSelect: (selectedItem) => {
+				const valueJSON = JSON.parse(selectedItem.value);
+				setSelectedSegmentEntry({
+					id: valueJSON.segmentsEntryId,
+					name: valueJSON.segmentsEntryName,
+				});
+			},
+			selectEventName: `${portletNamespace}selectSegmentsEntry`,
+			title: sub(
+				Liferay.Language.get('select-x'),
+				Liferay.Language.get('segment')
+			),
+			url: selectSegmentsEntryURL,
+		});
+	};
 
-		// TODO
-
-	}, []);
+	const handleMoreSegmentExperiencesButtonClick = () => {
+		openSelectionModal({
+			onSelect: (selectedItem) => {
+				const valueJSON = JSON.parse(selectedItem.value);
+				setSelectedSegmentsExperience(valueJSON);
+			},
+			selectEventName: `${portletNamespace}selectSegmentsExperience`,
+			title: Liferay.Language.get('select-experience'),
+			url: selectSegmentsExperienceURL,
+		});
+	};
 
 	useEffect(() => {
 		const deactivateSimulationEventHandler = Liferay.on(
@@ -301,7 +328,7 @@ function SegmentsAndExperiencesSelector({
 														false
 													);
 
-													handleMoreButtonClick();
+													handleMoreSegmentEntriesButtonClick();
 												}}
 											>
 												{Liferay.Language.get(
@@ -434,7 +461,7 @@ function SegmentsAndExperiencesSelector({
 														false
 													);
 
-													handleMoreButtonClick();
+													handleMoreSegmentExperiencesButtonClick();
 												}}
 											>
 												{Liferay.Language.get(
