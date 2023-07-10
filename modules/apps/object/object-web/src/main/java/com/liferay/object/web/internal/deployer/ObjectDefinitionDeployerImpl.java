27af3aad11b60d1c2e650aeb18b5b6d3a0245dc8
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-07-10 09:42:59 +0200

    LPS-190083 It is not necessary since we already have the object definition

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/deployer/ObjectDefinitionDeployerImpl.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/deployer/ObjectDefinitionDeployerImpl.java
@@ -311,7 +311,7 @@ public class ObjectDefinitionDeployerImpl implements ObjectDefinitionDeployer {
 				new ObjectEntryRowInfoItemRenderer(
 					_assetDisplayPageFriendlyURLProvider, _dlAppService,
 					_dlFileEntryLocalService, _dlURLHelper,
-					_listTypeEntryLocalService, _objectDefinitionLocalService,
+					_listTypeEntryLocalService, objectDefinition,
 					_objectEntryLocalService, _objectFieldLocalService,
 					_objectRelationshipLocalService, _portal, _servletContext),
 				HashMapDictionaryBuilder.<String, Object>put(
