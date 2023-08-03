2b0e7e5fd6eb8755c7500f93b8cd9634f8e551dd
Yurena Cabrera <yurena.cabrera@liferay.com>
2023-08-03 12:58:03 +0100

    LPS-192600 Create Display Page Template collection

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/portlet/action/AddDisplayPageFolderMVCActionCommand.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/portlet/action/AddDisplayPageFolderMVCActionCommand.java
@@ -1,12 +1,23 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
 package com.liferay.layout.page.template.admin.web.internal.portlet.action;
 
 import com.liferay.layout.page.template.admin.constants.LayoutPageTemplateAdminPortletKeys;
+import com.liferay.layout.page.template.service.LayoutPageTemplateCollectionService;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
-import org.osgi.service.component.annotations.Component;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Yurena Cabrera
@@ -22,8 +33,23 @@ public class AddDisplayPageFolderMVCActionCommand extends BaseMVCActionCommand {
 
 	@Override
 	protected void doProcessAction(
-		ActionRequest actionRequest, ActionResponse actionResponse)
+			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
 
+		_layoutPageTemplateCollectionService.addLayoutPageTemplateCollection(
+			_portal.getUserId(actionRequest),
+			ParamUtil.getString(actionRequest, "groupId"),
+			ParamUtil.getString(actionRequest, "name"),
+			ServiceContextFactory.getInstance(actionRequest));
+
+		sendRedirect(actionRequest, actionResponse);
 	}
+
+	@Reference
+	private LayoutPageTemplateCollectionService
+		_layoutPageTemplateCollectionService;
+
+	@Reference
+	private Portal _portal;
+
 }
