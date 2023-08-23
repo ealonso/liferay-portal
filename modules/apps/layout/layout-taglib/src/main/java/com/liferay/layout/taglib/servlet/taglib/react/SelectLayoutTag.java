e688618a0ae7d648425967108868c3cb2be5879a
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-08-23 23:35:16 +0100

    LPS-181477 Extract logic to a new class to reuse it

--- modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/servlet/taglib/react/SelectLayoutTag.java
+++ modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/servlet/taglib/react/SelectLayoutTag.java
@@ -5,35 +5,25 @@
 
 package com.liferay.layout.taglib.servlet.taglib.react;
 
-import com.liferay.item.selector.criteria.UUIDItemSelectorReturnType;
-import com.liferay.layout.item.selector.LayoutItemSelectorReturnType;
 import com.liferay.layout.taglib.internal.servlet.ServletContextUtil;
-import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.layout.taglib.internal.util.LayoutUtil;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
-import com.liferay.portal.kernel.security.auth.AuthTokenUtil;
 import com.liferay.portal.kernel.service.LayoutServiceUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.ArrayUtil;
-import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
-import com.liferay.portal.kernel.util.HttpComponentsUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.taglib.util.IncludeTag;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.PageContext;
@@ -146,7 +136,7 @@ public class SelectLayoutTag extends IncludeTag {
 					(ThemeDisplay)httpServletRequest.getAttribute(
 						WebKeys.THEME_DISPLAY);
 
-				return themeDisplay.getPathMain() + "/portal/get_layouts_tree";
+				return themeDisplay.getPathMain() + "/portal/get_layouts";
 			}
 		).put(
 			"maxPageSize",
@@ -191,111 +181,6 @@ public class SelectLayoutTag extends IncludeTag {
 		).build();
 	}
 
-	private JSONArray _getLayoutsJSONArray(
-			long groupId, boolean privateLayout, long parentLayoutId,
-			String[] selectedLayoutUuid)
-		throws Exception {
-
-		HttpServletRequest httpServletRequest = getRequest();
-
-		ThemeDisplay themeDisplay =
-			(ThemeDisplay)httpServletRequest.getAttribute(
-				WebKeys.THEME_DISPLAY);
-
-		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
-
-		List<Layout> layouts = LayoutServiceUtil.getLayouts(
-			groupId, privateLayout, parentLayoutId, false, QueryUtil.ALL_POS,
-			QueryUtil.ALL_POS);
-
-		for (Layout layout : layouts) {
-			if (_isExcludedLayout(layout)) {
-				continue;
-			}
-
-			jsonArray.put(
-				JSONUtil.put(
-					"children",
-					() -> {
-						JSONArray childrenJSONArray = _getLayoutsJSONArray(
-							groupId, privateLayout, layout.getLayoutId(),
-							selectedLayoutUuid);
-
-						if (childrenJSONArray.length() > 0) {
-							return childrenJSONArray;
-						}
-
-						return null;
-					}
-				).put(
-					"disabled",
-					() -> {
-						if ((_checkDisplayPage &&
-							 !layout.isContentDisplayPage()) ||
-							(_enableCurrentPage &&
-							 (layout.getPlid() == _getSelPlid()))) {
-
-							return true;
-						}
-
-						return null;
-					}
-				).put(
-					"groupId", layout.getGroupId()
-				).put(
-					"hasChildren", layout.hasChildren()
-				).put(
-					"icon", layout.getIcon()
-				).put(
-					"id", layout.getUuid()
-				).put(
-					"layoutId", layout.getLayoutId()
-				).put(
-					"name", layout.getName(themeDisplay.getLocale())
-				).put(
-					"paginated",
-					() -> {
-						int layoutsCount = LayoutServiceUtil.getLayoutsCount(
-							groupId, layout.isPrivateLayout(),
-							LayoutConstants.DEFAULT_PARENT_LAYOUT_ID);
-
-						if (layoutsCount >
-								PropsValues.
-									LAYOUT_MANAGE_PAGES_INITIAL_CHILDREN) {
-
-							return true;
-						}
-
-						return false;
-					}
-				).put(
-					"payload", _getPayload(layout, themeDisplay)
-				).put(
-					"privateLayout", layout.isPrivateLayout()
-				).put(
-					"returnType", getItemSelectorReturnType()
-				).put(
-					"selected",
-					() -> {
-						if (ArrayUtil.contains(
-								selectedLayoutUuid, layout.getUuid())) {
-
-							return true;
-						}
-
-						return false;
-					}
-				).put(
-					"url",
-					PortalUtil.getLayoutRelativeURL(layout, themeDisplay, false)
-				).put(
-					"value", layout.getBreadcrumb(themeDisplay.getLocale())
-				));
-		}
-
-		return jsonArray;
-	}
-
 	private JSONArray _getLayoutsJSONArray(String[] selectedLayoutIds)
 		throws Exception {
 
@@ -316,8 +201,10 @@ public class SelectLayoutTag extends IncludeTag {
 		return JSONUtil.put(
 			JSONUtil.put(
 				"children",
-				_getLayoutsJSONArray(
-					themeDisplay.getScopeGroupId(), _privateLayout, 0,
+				LayoutUtil.getLayoutsJSONArray(
+					_checkDisplayPage, themeDisplay.getScopeGroupId(),
+					httpServletRequest, _itemSelectorReturnType,
+					LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, _privateLayout,
 					selectedLayoutIds)
 			).put(
 				"disabled", true
@@ -331,70 +218,22 @@ public class SelectLayoutTag extends IncludeTag {
 				"id", "0"
 			).put(
 				"name", themeDisplay.getScopeGroupName()
-			));
-	}
-
-	private String _getPayload(Layout layout, ThemeDisplay themeDisplay)
-		throws Exception {
-
-		if (Objects.equals(
-				LayoutItemSelectorReturnType.class.getName(),
-				getItemSelectorReturnType())) {
-
-			return JSONUtil.put(
-				"layoutId", layout.getLayoutId()
 			).put(
-				"name", layout.getName(themeDisplay.getLocale())
-			).put(
-				"plid", layout.getPlid()
-			).put(
-				"previewURL",
+				"paginated",
 				() -> {
-					String layoutURL = HttpComponentsUtil.addParameter(
-						PortalUtil.getLayoutFullURL(layout, themeDisplay),
-						"p_l_mode", Constants.PREVIEW);
+					int layoutsCount = LayoutServiceUtil.getLayoutsCount(
+						themeDisplay.getScopeGroupId(), _privateLayout,
+						LayoutConstants.DEFAULT_PARENT_LAYOUT_ID);
 
-					return HttpComponentsUtil.addParameter(
-						layoutURL, "p_p_auth",
-						AuthTokenUtil.getToken(getRequest()));
+					if (layoutsCount >
+							PropsValues.LAYOUT_MANAGE_PAGES_INITIAL_CHILDREN) {
+
+						return true;
+					}
+
+					return false;
 				}
-			).put(
-				"private", layout.isPrivateLayout()
-			).put(
-				"url", PortalUtil.getLayoutFullURL(layout, themeDisplay)
-			).put(
-				"uuid", layout.getUuid()
-			).toString();
-		}
-		else if (Objects.equals(
-					UUIDItemSelectorReturnType.class.getName(),
-					getItemSelectorReturnType())) {
-
-			return layout.getUuid();
-		}
-
-		return PortalUtil.getLayoutRelativeURL(layout, themeDisplay, false);
-	}
-
-	private long _getSelPlid() {
-		return ParamUtil.getLong(
-			getRequest(), "selPlid", LayoutConstants.DEFAULT_PLID);
-	}
-
-	private boolean _isExcludedLayout(Layout layout) {
-		if (!layout.isTypeContent()) {
-			return false;
-		}
-
-		if (layout.fetchDraftLayout() != null) {
-			return !layout.isPublished();
-		}
-
-		if (layout.isApproved() && !layout.isHidden() && !layout.isSystem()) {
-			return false;
-		}
-
-		return true;
+			));
 	}
 
 	private static final String _PAGE = "/select_layout/page.jsp";
