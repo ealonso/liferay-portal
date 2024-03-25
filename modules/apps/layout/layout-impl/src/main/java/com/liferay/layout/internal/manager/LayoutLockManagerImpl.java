501a86f6966bbc69c5131466ed4a7a565bd5652b
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-03-25 10:05:24 +0100

    LPD-21461 Take into account new controller type, TYPE_UTILITY

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/manager/LayoutLockManagerImpl.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/manager/LayoutLockManagerImpl.java
@@ -13,8 +13,6 @@ import com.liferay.layout.model.LockedLayoutType;
 import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalService;
-import com.liferay.layout.utility.page.model.LayoutUtilityPageEntry;
-import com.liferay.layout.utility.page.service.LayoutUtilityPageEntryLocalService;
 import com.liferay.petra.sql.dsl.Column;
 import com.liferay.petra.sql.dsl.DSLFunctionFactoryUtil;
 import com.liferay.petra.sql.dsl.DSLQueryFactoryUtil;
@@ -162,7 +160,8 @@ public class LayoutLockManagerImpl implements LayoutLockManager {
 							new String[] {
 								LayoutConstants.TYPE_ASSET_DISPLAY,
 								LayoutConstants.TYPE_COLLECTION,
-								LayoutConstants.TYPE_CONTENT
+								LayoutConstants.TYPE_CONTENT,
+								LayoutConstants.TYPE_UTILITY
 							})
 					)
 				).as(
@@ -486,6 +485,10 @@ public class LayoutLockManagerImpl implements LayoutLockManager {
 			return LockedLayoutType.COLLECTION_PAGE;
 		}
 
+		if (Objects.equals(type, LayoutConstants.TYPE_UTILITY)) {
+			return LockedLayoutType.UTILITY_PAGE;
+		}
+
 		if (!Objects.equals(type, LayoutConstants.TYPE_CONTENT)) {
 			return null;
 		}
@@ -497,14 +500,6 @@ public class LayoutLockManagerImpl implements LayoutLockManager {
 		if (layoutPageTemplateEntry != null) {
 			return _getLayoutPageTemplateEntryTypeLabel(
 				layoutPageTemplateEntry);
-		}
-
-		LayoutUtilityPageEntry layoutUtilityPageEntry =
-			_layoutUtilityPageEntryLocalService.
-				fetchLayoutUtilityPageEntryByPlid(classPK);
-
-		if (layoutUtilityPageEntry != null) {
-			return LockedLayoutType.UTILITY_PAGE;
 		}
 
 		return LockedLayoutType.CONTENT_PAGE;
@@ -525,10 +520,6 @@ public class LayoutLockManagerImpl implements LayoutLockManager {
 	@Reference
 	private LayoutPageTemplateEntryLocalService
 		_layoutPageTemplateEntryLocalService;
-
-	@Reference
-	private LayoutUtilityPageEntryLocalService
-		_layoutUtilityPageEntryLocalService;
 
 	private volatile long _lockExpirationTime;
 
