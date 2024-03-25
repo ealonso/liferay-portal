ac8468477770b377110de090339f884dfa18f2d6
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-03-25 10:21:26 +0100

    LPD-21447 Simplify

--- modules/apps/layout/layout-utility-page/layout-utility-page-create-account/src/main/java/com/liferay/layout/utility/page/create/account/internal/CreateAccountLayoutUtilityPageEntryViewRenderer.java
+++ modules/apps/layout/layout-utility-page/layout-utility-page-create-account/src/main/java/com/liferay/layout/utility/page/create/account/internal/CreateAccountLayoutUtilityPageEntryViewRenderer.java
@@ -9,27 +9,20 @@ import com.liferay.layout.utility.page.kernel.LayoutUtilityPageEntryViewRenderer
 import com.liferay.layout.utility.page.kernel.constants.LayoutUtilityPageEntryConstants;
 import com.liferay.portal.kernel.language.Language;
 
-import java.io.IOException;
-
 import java.util.Locale;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Alvaro Saugar
  */
+@Component(
+	property = "utility.page.type=" + LayoutUtilityPageEntryConstants.TYPE_CREATE_ACCOUNT,
+	service = LayoutUtilityPageEntryViewRenderer.class
+)
 public class CreateAccountLayoutUtilityPageEntryViewRenderer
 	implements LayoutUtilityPageEntryViewRenderer {
-
-	public CreateAccountLayoutUtilityPageEntryViewRenderer(
-		Language language, ServletContext servletContext) {
-
-		_language = language;
-		_servletContext = servletContext;
-	}
 
 	@Override
 	public String getLabel(Locale locale) {
@@ -41,14 +34,7 @@ public class CreateAccountLayoutUtilityPageEntryViewRenderer
 		return LayoutUtilityPageEntryConstants.TYPE_CREATE_ACCOUNT;
 	}
 
-	@Override
-	public void renderHTML(
-			HttpServletRequest httpServletRequest,
-			HttpServletResponse httpServletResponse)
-		throws IOException, ServletException {
-	}
-
-	private final Language _language;
-	private final ServletContext _servletContext;
+	@Reference
+	private Language _language;
 
 }
