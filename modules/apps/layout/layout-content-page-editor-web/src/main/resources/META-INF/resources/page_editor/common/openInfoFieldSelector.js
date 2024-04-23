9df3c8d61d94412167c3d164010af91b5fab4aa0
Sandro Chinea <sandrochinea@gmail.com>
2024-04-23 13:37:17 +0100

    LPD-23904 Create function to call the new item selector
    We are using image selector URL temporarily until we have the new item selector ready

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/common/openInfoFieldSelector.js
@@ -0,0 +1,24 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {getPortletNamespace, openSelectionModal} from 'frontend-js-web';
+
+import {config} from '../app/config/index';
+
+export function openInfoFieldSelector({itemType, onCancel, onSave}) {
+	const url = new URL(config.infoFieldItemSelectorURL);
+
+	url.searchParams.set(
+		`${getPortletNamespace(Liferay.PortletKeys.ITEM_SELECTOR)}itemType`,
+		itemType
+	);
+
+	openSelectionModal({
+		onClose: onCancel,
+		onSelect: onSave,
+		title: Liferay.Language.get('manage-form-fields'),
+		url: url.toString(),
+	});
+}
