2fa42480d63d965beeee1b48c42470a6ea3e922c
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-09-03 20:23:04 +0200

    LPD-35529 Migrates NestWidgetInsideNestedApplications to playwright

---
+++ modules/test/playwright/tests/nested-portlets-web/nestedPortletsWidget.spec.ts
@@ -0,0 +1,87 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {expect, mergeTests} from '@playwright/test';
+
+import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
+import {isolatedSiteTest} from '../../fixtures/isolatedSiteTest';
+import {loginTest} from '../../fixtures/loginTest';
+import {pageViewModePagesTest} from '../../fixtures/pageViewModePagesTest';
+import getRandomString from '../../utils/getRandomString';
+
+const test = mergeTests(
+	apiHelpersTest,
+	isolatedSiteTest,
+	loginTest(),
+	pageViewModePagesTest
+);
+
+test('User can nest a widget inside nested portlets widget', async ({
+	apiHelpers,
+	page,
+	site,
+	widgetPagePage,
+}) => {
+
+	// Add widget page and navigate to view
+
+	const layout = await apiHelpers.jsonWebServicesLayout.addLayout({
+		groupId: site.id,
+		title: getRandomString(),
+	});
+
+	await page.goto(`/web${site.friendlyUrlPath}${layout.friendlyURL}`);
+
+	// Add nested applications widget and assert default template
+
+	await widgetPagePage.addPortlet('Nested Applications');
+
+	await page
+		.locator('.portlet-nested-portlets')
+		.first()
+		.getByLabel('Options')
+		.click();
+
+	await page
+		.getByRole('menuitem', {exact: true, name: 'Configuration'})
+		.click();
+
+	const configurationIFrame = page.frameLocator(
+		'iframe[title*="Nested Applications"]'
+	);
+
+	await expect(
+		configurationIFrame.getByLabel('2 Columns (50/50)')
+	).toBeChecked();
+
+	await page
+		.locator('.modal-header')
+		.getByLabel('close', {exact: true})
+		.click();
+
+	// Add web content display widget and drag into nested applications widget
+
+	await widgetPagePage.addPortlet('Web Content Display');
+
+	const webContentDisplayTopper = page.locator(
+		'.portlet-journal-content .portlet-topper'
+	);
+
+	await webContentDisplayTopper.waitFor({state: 'visible'});
+
+	await webContentDisplayTopper.dragTo(
+		page.locator(
+			'.portlet-nested-portlets .portlet-column-first.yui3-dd-drop.yui3.dd-drop-over'
+		)
+	);
+
+	await expect(
+		page
+			.locator(
+				'.portlet-nested-portlets .portlet-column-content-first .portlet-title-default'
+			)
+			.getByText('Web Content Display')
+	).toBeAttached();
+});
