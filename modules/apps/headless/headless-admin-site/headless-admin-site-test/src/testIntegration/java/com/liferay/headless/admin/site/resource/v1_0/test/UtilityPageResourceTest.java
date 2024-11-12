8667e9d2ee46ca038acb7a5a05a587263351682e
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-11-12 14:07:22 +0000

    LPD-41548 Ignore not implemented tests

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/UtilityPageResourceTest.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/UtilityPageResourceTest.java
@@ -6,14 +6,123 @@
 package com.liferay.headless.admin.site.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.test.rule.FeatureFlags;
 
 import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Rubén Pulido
  */
-@Ignore
+@FeatureFlags("LPD-35443")
 @RunWith(Arquillian.class)
 public class UtilityPageResourceTest extends BaseUtilityPageResourceTestCase {
+
+	@Ignore
+	@Override
+	@Test
+	public void testGetSiteSiteByExternalReferenceCodeUtilityPagePermissionsPage()
+		throws Exception {
+
+		super.
+			testGetSiteSiteByExternalReferenceCodeUtilityPagePermissionsPage();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGetSiteSiteByExternalReferenceCodeUtilityPagesPageWithPagination()
+		throws Exception {
+
+		super.
+			testGetSiteSiteByExternalReferenceCodeUtilityPagesPageWithPagination();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGetSiteSiteByExternalReferenceCodeUtilityPagesPageWithSortDateTime()
+		throws Exception {
+
+		super.
+			testGetSiteSiteByExternalReferenceCodeUtilityPagesPageWithSortDateTime();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGetSiteSiteByExternalReferenceCodeUtilityPagesPageWithSortDouble()
+		throws Exception {
+
+		super.
+			testGetSiteSiteByExternalReferenceCodeUtilityPagesPageWithSortDouble();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGetSiteSiteByExternalReferenceCodeUtilityPagesPageWithSortInteger()
+		throws Exception {
+
+		super.
+			testGetSiteSiteByExternalReferenceCodeUtilityPagesPageWithSortInteger();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGetSiteSiteByExternalReferenceCodeUtilityPagesPageWithSortString()
+		throws Exception {
+
+		super.
+			testGetSiteSiteByExternalReferenceCodeUtilityPagesPageWithSortString();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGetSiteSiteExternalReferenceCodeUtilityPagePermissionsPage()
+		throws Exception {
+
+		super.testGetSiteSiteExternalReferenceCodeUtilityPagePermissionsPage();
+	}
+
+	@Override
+	@Test
+	public void testPatchSiteSiteByExternalReferenceCodeUtilityPage()
+		throws Exception {
+
+		super.testPatchSiteSiteByExternalReferenceCodeUtilityPage();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testPostSiteSiteByExternalReferenceCodeUtilityPagePageSpecification()
+		throws Exception {
+
+		super.
+			testPostSiteSiteByExternalReferenceCodeUtilityPagePageSpecification();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testPutSiteSiteByExternalReferenceCodeUtilityPagePermissionsPage()
+		throws Exception {
+
+		super.
+			testPutSiteSiteByExternalReferenceCodeUtilityPagePermissionsPage();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testPutSiteSiteExternalReferenceCodeUtilityPagePermissionsPage()
+		throws Exception {
+
+		super.testPutSiteSiteExternalReferenceCodeUtilityPagePermissionsPage();
+	}
+
 }
