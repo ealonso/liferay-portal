641090038419f830d5cb8143551e6ff466ccc1c1
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-11-12 11:56:52 +0000

    LPD-41548 Adds Get UtilityPages endpoint

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/UtilityPageResourceImpl.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/UtilityPageResourceImpl.java
@@ -5,9 +5,22 @@
 
 package com.liferay.headless.admin.site.internal.resource.v1_0;
 
+import com.liferay.headless.admin.site.dto.v1_0.UtilityPage;
 import com.liferay.headless.admin.site.resource.v1_0.UtilityPageResource;
+import com.liferay.layout.utility.page.model.LayoutUtilityPageEntry;
+import com.liferay.layout.utility.page.service.LayoutUtilityPageEntryService;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.search.filter.Filter;
+import com.liferay.portal.vulcan.aggregation.Aggregation;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -18,4 +31,37 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = UtilityPageResource.class
 )
 public class UtilityPageResourceImpl extends BaseUtilityPageResourceImpl {
+
+	@Override
+	public Page<UtilityPage> getSiteSiteByExternalReferenceCodeUtilityPagesPage(
+			String siteExternalReferenceCode, String search,
+			Aggregation aggregation, Filter filter, Pagination pagination,
+			Sort[] sorts)
+		throws Exception {
+
+		if (!FeatureFlagManagerUtil.isEnabled("LPD-35443")) {
+			throw new UnsupportedOperationException();
+		}
+
+		Group group = groupLocalService.getGroupByExternalReferenceCode(
+			siteExternalReferenceCode, contextCompany.getCompanyId());
+
+		return Page.of(
+			transform(
+				_layoutUtilityPageEntryService.getLayoutUtilityPageEntries(
+					group.getGroupId(), QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+					null),
+				layoutUtilityPageEntry -> _utilityPageDTOConverter.toDTO(
+					layoutUtilityPageEntry)));
+	}
+
+	@Reference
+	private LayoutUtilityPageEntryService _layoutUtilityPageEntryService;
+
+	@Reference(
+		target = "(component.name=com.liferay.headless.admin.site.internal.dto.v1_0.converter.UtilityPageDTOConverter)"
+	)
+	private DTOConverter<LayoutUtilityPageEntry, UtilityPage>
+		_utilityPageDTOConverter;
+
 }
