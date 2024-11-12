0c219c256d3f56b9625a4ca7d5b141696223cd6f
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-11-12 12:24:59 +0000

    LPD-41548 Adds POST UtilityPage endpoint

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/UtilityPageResourceImpl.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/UtilityPageResourceImpl.java
@@ -7,6 +7,8 @@ package com.liferay.headless.admin.site.internal.resource.v1_0;
 
 import com.liferay.headless.admin.site.dto.v1_0.UtilityPage;
 import com.liferay.headless.admin.site.resource.v1_0.UtilityPageResource;
+import com.liferay.headless.common.spi.service.context.ServiceContextBuilder;
+import com.liferay.layout.utility.page.kernel.constants.LayoutUtilityPageEntryConstants;
 import com.liferay.layout.utility.page.model.LayoutUtilityPageEntry;
 import com.liferay.layout.utility.page.service.LayoutUtilityPageEntryService;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
@@ -14,10 +16,14 @@ import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.search.filter.Filter;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.vulcan.aggregation.Aggregation;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.portal.vulcan.pagination.Pagination;
+
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -54,6 +60,73 @@ public class UtilityPageResourceImpl extends BaseUtilityPageResourceImpl {
 				layoutUtilityPageEntry -> _utilityPageDTOConverter.toDTO(
 					layoutUtilityPageEntry)));
 	}
+
+	@Override
+	public UtilityPage postSiteSiteByExternalReferenceCodeUtilityPage(
+			String siteExternalReferenceCode, UtilityPage utilityPage)
+		throws Exception {
+
+		if (!FeatureFlagManagerUtil.isEnabled("LPD-35443")) {
+			throw new UnsupportedOperationException();
+		}
+
+		Group group = groupLocalService.getGroupByExternalReferenceCode(
+			siteExternalReferenceCode, contextCompany.getCompanyId());
+
+		return _utilityPageDTOConverter.toDTO(
+			_layoutUtilityPageEntryService.addLayoutUtilityPageEntry(
+				utilityPage.getExternalReferenceCode(), group.getGroupId(), 0L,
+				0L, utilityPage.getMarkedAsDefault(), utilityPage.getName(),
+				_getType(utilityPage.getType()), 0L,
+				_getServiceContext(group, utilityPage)));
+	}
+
+	private ServiceContext _getServiceContext(
+		Group group, UtilityPage utilityPage) {
+
+		ServiceContext serviceContext = ServiceContextBuilder.create(
+			group.getGroupId(), contextHttpServletRequest, null
+		).build();
+
+		serviceContext.setCreateDate(utilityPage.getDateCreated());
+		serviceContext.setModifiedDate(utilityPage.getDateModified());
+		serviceContext.setUuid(utilityPage.getUuid());
+
+		return serviceContext;
+	}
+
+	private String _getType(UtilityPage.Type type) {
+		if (_externalToInternalTypeValuesMap.containsKey(type)) {
+			return _externalToInternalTypeValuesMap.get(type);
+		}
+
+		throw new UnsupportedOperationException();
+	}
+
+	private static final Map<UtilityPage.Type, String>
+		_externalToInternalTypeValuesMap = HashMapBuilder.put(
+			UtilityPage.Type.COOKIE_POLICY,
+			LayoutUtilityPageEntryConstants.TYPE_COOKIE_POLICY
+		).put(
+			UtilityPage.Type.CREATE_ACCOUNT,
+			LayoutUtilityPageEntryConstants.TYPE_CREATE_ACCOUNT
+		).put(
+			UtilityPage.Type.ERROR, LayoutUtilityPageEntryConstants.TYPE_STATUS
+		).put(
+			UtilityPage.Type.ERROR_CODE404,
+			LayoutUtilityPageEntryConstants.TYPE_SC_NOT_FOUND
+		).put(
+			UtilityPage.Type.ERROR_CODE500,
+			LayoutUtilityPageEntryConstants.TYPE_SC_INTERNAL_SERVER_ERROR
+		).put(
+			UtilityPage.Type.FORGOT_PASSWORD,
+			LayoutUtilityPageEntryConstants.TYPE_FORGOT_PASSWORD
+		).put(
+			UtilityPage.Type.LOGIN, LayoutUtilityPageEntryConstants.TYPE_LOGIN
+		).put(
+			UtilityPage.Type.TERMS_OF_USE,
+			LayoutUtilityPageEntryConstants.TYPE_TERMS_OF_USE
+		).build();
 
 	@Reference
 	private LayoutUtilityPageEntryService _layoutUtilityPageEntryService;
