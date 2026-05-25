7312a7310bc62eefed83a83bfcc9215a5debd01e
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2026-05-25 14:05:11 +0100

    LPD-91690 Propagate memberLookup through provider registry

--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/provider/SegmentsEntryProviderRegistryImpl.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/provider/SegmentsEntryProviderRegistryImpl.java
@@ -74,7 +74,7 @@ public class SegmentsEntryProviderRegistryImpl
 		}
 
 		return segmentsEntryProvider.getSegmentsEntryClassPKs(
-			segmentsEntryId, start, end);
+			segmentsEntryId, start, end, true);
 	}
 
 	@Override
@@ -108,7 +108,7 @@ public class SegmentsEntryProviderRegistryImpl
 		}
 
 		return segmentsEntryProvider.getSegmentsEntryClassPKsCount(
-			segmentsEntryId);
+			segmentsEntryId, true);
 	}
 
 	@Override
