b9df1083b0ca8985654ce0efb0fed91e77f15e50
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2026-06-09 12:46:59 +0100

    LPD-92183 Add newToken coverage to OAuth2FaroControllerTest

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/test/java/com/liferay/osb/faro/web/internal/controller/main/OAuth2FaroControllerTest.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/test/java/com/liferay/osb/faro/web/internal/controller/main/OAuth2FaroControllerTest.java
@@ -5,14 +5,26 @@
 
 package com.liferay.osb.faro.web.internal.controller.main;
 
+import com.liferay.expando.kernel.model.ExpandoBridge;
+import com.liferay.oauth.client.LocalOAuthClient;
+import com.liferay.oauth2.provider.model.OAuth2Application;
 import com.liferay.oauth2.provider.model.OAuth2Authorization;
+import com.liferay.oauth2.provider.service.OAuth2ApplicationLocalService;
 import com.liferay.oauth2.provider.service.OAuth2AuthorizationService;
+import com.liferay.osb.faro.web.internal.model.display.main.TokenDisplay;
+import com.liferay.portal.json.JSONFactoryImpl;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -35,8 +47,65 @@ public class OAuth2FaroControllerTest {
 	@Before
 	public void setUp() {
 		ReflectionTestUtils.setField(
+			_oAuth2FaroController, "_jsonFactory", new JSONFactoryImpl());
+		ReflectionTestUtils.setField(
+			_oAuth2FaroController, "_localOAuthClient", _localOAuthClient);
+		ReflectionTestUtils.setField(
+			_oAuth2FaroController, "_oAuth2ApplicationLocalService",
+			_oAuth2ApplicationLocalService);
+		ReflectionTestUtils.setField(
 			_oAuth2FaroController, "_oAuth2AuthorizationService",
 			_oAuth2AuthorizationService);
+
+		PermissionChecker permissionChecker = Mockito.mock(
+			PermissionChecker.class);
+
+		Mockito.when(
+			permissionChecker.getUser()
+		).thenReturn(
+			Mockito.mock(User.class)
+		);
+
+		PermissionThreadLocal.setPermissionChecker(permissionChecker);
+	}
+
+	@Test
+	public void testNewToken() throws Exception {
+		OAuth2Application oAuth2Application = _mockOAuth2Application();
+
+		Mockito.when(
+			_localOAuthClient.requestTokens(oAuth2Application, 100L)
+		).thenReturn(
+			"{\"access_token\": \"abc\"}"
+		);
+
+		OAuth2Authorization oAuth2Authorization = _mockOAuth2Authorization(
+			"abc");
+
+		Mockito.when(
+			_oAuth2AuthorizationService.getUserOAuth2Authorizations(
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS, null)
+		).thenReturn(
+			Arrays.asList(oAuth2Authorization)
+		);
+
+		TokenDisplay tokenDisplay = _oAuth2FaroController.newToken(
+			1, null, "demandbase", null);
+
+		Assert.assertEquals("abc", tokenDisplay.getToken());
+	}
+
+	@Test(expected = PortalException.class)
+	public void testNewTokenWhenAccessTokenIsNotCreated() throws Exception {
+		OAuth2Application oAuth2Application = _mockOAuth2Application();
+
+		Mockito.when(
+			_localOAuthClient.requestTokens(oAuth2Application, 100L)
+		).thenReturn(
+			null
+		);
+
+		_oAuth2FaroController.newToken(1, null, "demandbase", null);
 	}
 
 	@Test
@@ -88,6 +157,82 @@ public class OAuth2FaroControllerTest {
 		_oAuth2FaroController.revokeToken(1, "nonexistent");
 	}
 
+	private OAuth2Application _mockOAuth2Application() throws Exception {
+		OAuth2Application oAuth2Application = Mockito.mock(
+			OAuth2Application.class);
+
+		Mockito.when(
+			_oAuth2ApplicationLocalService.fetchOAuth2Application(
+				Mockito.anyLong(), Mockito.eq("DEMANDBASE"))
+		).thenReturn(
+			oAuth2Application
+		);
+
+		Mockito.when(
+			oAuth2Application.getClientCredentialUserId()
+		).thenReturn(
+			100L
+		);
+
+		Mockito.when(
+			oAuth2Application.getOAuth2ApplicationId()
+		).thenReturn(
+			200L
+		);
+
+		Mockito.when(
+			_oAuth2AuthorizationService.getApplicationOAuth2Authorizations(
+				200L, QueryUtil.ALL_POS, QueryUtil.ALL_POS, null)
+		).thenReturn(
+			Collections.emptyList()
+		);
+
+		return oAuth2Application;
+	}
+
+	private OAuth2Authorization _mockOAuth2Authorization(String accessToken) {
+		OAuth2Authorization oAuth2Authorization = Mockito.mock(
+			OAuth2Authorization.class);
+
+		Mockito.when(
+			oAuth2Authorization.getAccessTokenContent()
+		).thenReturn(
+			accessToken
+		);
+
+		Date date = new Date();
+
+		Mockito.when(
+			oAuth2Authorization.getAccessTokenCreateDate()
+		).thenReturn(
+			date
+		);
+
+		Mockito.when(
+			oAuth2Authorization.getAccessTokenExpirationDate()
+		).thenReturn(
+			date
+		);
+
+		Mockito.when(
+			oAuth2Authorization.getCreateDate()
+		).thenReturn(
+			date
+		);
+
+		Mockito.when(
+			oAuth2Authorization.getExpandoBridge()
+		).thenReturn(
+			Mockito.mock(ExpandoBridge.class)
+		);
+
+		return oAuth2Authorization;
+	}
+
+	private final LocalOAuthClient _localOAuthClient = Mockito.mock(
+		LocalOAuthClient.class);
+	private final OAuth2ApplicationLocalService _oAuth2ApplicationLocalService =
+		Mockito.mock(OAuth2ApplicationLocalService.class);
 	private final OAuth2AuthorizationService _oAuth2AuthorizationService =
 		Mockito.mock(OAuth2AuthorizationService.class);
 	private final OAuth2FaroController _oAuth2FaroController =
