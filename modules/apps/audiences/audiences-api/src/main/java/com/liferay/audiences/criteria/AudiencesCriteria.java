90ef444914cc7a6e17b50aba6594097fc2d3d9f7
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2026-06-29 13:13:50 +0100

    LPD-96093 Adds new InputType to define the type of input field in the UI, also adapt Type to define the supported types in frontend-js-audiences

--- modules/apps/audiences/audiences-api/src/main/java/com/liferay/audiences/criteria/AudiencesCriteria.java
+++ modules/apps/audiences/audiences-api/src/main/java/com/liferay/audiences/criteria/AudiencesCriteria.java
@@ -13,22 +13,29 @@ import java.util.List;
 public class AudiencesCriteria {
 
 	public AudiencesCriteria(
-		String icon, String key, String label, List<Option> options,
-		Type type) {
+		String icon, InputType inputType, String key, String label,
+		List<Option> options, Type type) {
 
 		_icon = icon;
+		_inputType = inputType;
 		_key = key;
 		_label = label;
 		_options = options;
 		_type = type;
 	}
 
-	public AudiencesCriteria(String icon, String key, String label, Type type) {
-		this(icon, key, label, null, type);
+	public AudiencesCriteria(
+		String icon, InputType inputType, String key, String label, Type type) {
+
+		this(icon, inputType, key, label, null, type);
 	}
 
 	public String getIcon() {
 		return _icon;
+	}
+
+	public InputType getInputType() {
+		return _inputType;
 	}
 
 	public String getKey() {
@@ -67,13 +74,40 @@ public class AudiencesCriteria {
 
 	}
 
+	public enum InputType {
+
+		BOOLEAN("boolean"), DATE("date"), SELECT("select"), TEXT("text");
+
+		public String getValue() {
+			return _value;
+		}
+
+		private InputType(String value) {
+			_value = value;
+		}
+
+		private final String _value;
+
+	}
+
 	public enum Type {
 
-		COLLECTION, DATE, NUMBER, OPTIONS, STRING;
+		BOOLEAN("boolean"), NUMBER("number"), SET("set"), STRING("string");
+
+		public String getValue() {
+			return _value;
+		}
+
+		private Type(String value) {
+			_value = value;
+		}
+
+		private final String _value;
 
 	}
 
 	private final String _icon;
+	private final InputType _inputType;
 	private final String _key;
 	private final String _label;
 	private final List<Option> _options;
